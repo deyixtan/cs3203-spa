5278a8b0ab7a643981aa6f1d49e3e2097ff57f0f
Deyi Tan <deyixtan@gmail.com>
2022-01-29 22:55:14 +0800

    Update autotester to link to QueryController

--- Team20/Code20/src/spa/src/components/query_subsystem/QueryController.cpp
+++ Team20/Code20/src/spa/src/components/query_subsystem/QueryController.cpp
@@ -1,3 +1,5 @@
 #include "QueryController.h"
 
 QueryController::QueryController() {}
+
+void QueryController::ProcessQuery(std::string query, std::list<std::string>& results) {}
--- Team20/Code20/src/spa/src/components/query_subsystem/QueryController.h
+++ Team20/Code20/src/spa/src/components/query_subsystem/QueryController.h
@@ -1,10 +1,13 @@
 #ifndef SPA_QUERYCONTROLLER_H
 #define SPA_QUERYCONTROLLER_H
 
+#include <list>
+#include <string>
 
 class QueryController {
 public:
     QueryController();
+    void ProcessQuery(std::string query, std::list<std::string>& results);
 };
 
 
