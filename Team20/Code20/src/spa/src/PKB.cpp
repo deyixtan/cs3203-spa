dae8bda7b2b1ad042025ab0549c67499729f167b
Jaryl Loh <jarylloh1997@gmail.com>
2022-02-01 13:05:01 +0800

    feat: Create skeleton for PKB

--- Team20/Code20/src/spa/src/PKB.cpp
+++ Team20/Code20/src/spa/src/PKB.cpp
@@ -1,17 +1,123 @@
-#include<stdio.h>
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
-
-using namespace std;
+#include <unordered_set>
+#include <unordered_map>
 
 #include "PKB.h"
 #include "TNode.h"
 
+/*
 int PKB::setProcToAST(PROC p, TNode* r) {
 	return 0;
 }
 
 TNode* PKB::getRootAST (PROC p){
 	return nullptr;
+}
+*/
+
+//Keywords = [procedure, while, read, print, call, if-else-then, assign] -> Need to maintain 1 list for each of these
+
+std::unordered_set<std::string> PKB::procList;
+std::unordered_set<int> PKB::whileStmtList;
+std::unordered_set<int> PKB::readStmtList;
+std::unordered_set<int> PKB::printStmtList;
+std::unordered_set<int> PKB::callStmtList;
+std::unordered_set<int> PKB::ifStmtList; //need to create separate else-then or treat this as a block?
+std::unordered_set<int> PKB::assignStmtList;
+
+std::unordered_set<std::string> PKB::varList;
+std::unordered_set<std::string> PKB::constList;
+
+//FollowStore PKB::followStore;
+//ParentStore PKB::parentStore;
+//UsageStore PKB::usageStore;
+//ModifyStore PKB::modifyStore;
+
+PKB::PKB() {}
+
+/* Adders */
+
+void PKB::addProcStmt(int stmtNo, std::string procedure) {
+    //code goes here
+}
+
+void PKB::addWhileStmt(int stmtNo, std::string procedure) {
+    //code goes here
+}
+
+void PKB::addReadStmt(int stmtNo, std::string procedure) {
+    //code goes here
+}
+
+void PKB::addPrintStmt(int stmtNo, std::string procedure) {
+    //code goes here
+}
+
+void PKB::addCallStmt(int stmtNo, std::string procedure) {
+    //code goes here
+}
+
+void PKB::addIfStmt(int stmtNo, std::string procedure) {
+    //code goes here
+}
+
+void PKB::addAssignStmt(int stmtNo, std::string procedure) {
+    //code goes here
+}
+
+void PKB::addVar(std::string name) {
+
+}
+
+void PKB::addConst(std::string value) {
+    //code goes here
+}
+
+bool PKB::addUsageStmt(int stmt, std::string variable) {
+    //code goes here
+}
+
+bool PKB::addModifyStmt(int stmt, std::string variable) {
+    //code goes here
+}
+
+/* Getters */
+
+std::unordered_set<int> PKB::getProcStmts() {
+    //code goes here
+}
+
+std::unordered_set<int> PKB::getWhileStmts() {
+    //code goes here
+}
+
+std::unordered_set<int> PKB::getReadStmts() {
+    //code goes here
+}
+
+std::unordered_set<int> PKB::getPrintStmts() {
+    //code goes here
+}
+
+std::unordered_set<int> PKB::getCallStmts() {
+    //code goes here
+}
+
+std::unordered_set<int> PKB::getIfStmts() {
+    //code goes here
+}
+
+std::unordered_set<int> PKB::getAssignStmts() {
+    //code goes here
+}
+
+std::unordered_set<std::string> PKB::getVars() {
+    //code goes here
+}
+
+std::unordered_set<std::string> PKB::getConsts() {
+    //code goes here
 }
--- Team20/Code20/src/spa/src/PKB.h
+++ Team20/Code20/src/spa/src/PKB.h
@@ -1,21 +1,85 @@
 #pragma once
 
-#include<stdio.h>
-#include <iostream>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
-using namespace std;
-typedef short PROC;
+//#include "FollowStorage.h"
+//#include "ParentStorage.h"
+//#include "UseStorage.h"
+//#include "ModifyStorage.h"
 
-class TNode;
+//typedef short PROC;
 
-class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
+//class TNode;
+
+//class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
 public:
-	static VarTable* varTable; 
-	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
+	//static VarTable* varTable;
+	//static int setProcToAST(PROC p, TNode* r);
+	//static TNode* getRootAST (PROC p);
+    PKB();
 
+    /* Adders */
+
+    void addProcStmt(int stmtNo, std::string procedure);
+
+    void addWhileStmt(int stmtNo, std::string procedure);
+
+    void addReadStmt(int stmtNo, std::string procedure);
+
+    void addPrintStmt(int stmtNo, std::string procedure);
+
+    void addCallStmt(int stmtNo, std::string procedure);
+
+    void addIfStmt(int stmtNo, std::string procedure);
+
+    void addAssignStmt(int stmtNo, std::string procedure);
+
+    void addVar(std::string name);
+
+    void addConst(std::string value);
+
+    bool addUsageStmt(int stmt, std::string variable);
+
+    bool addModifyStmt(int stmt, std::string variable);
+
+    /* Getters */
+
+    std::unordered_set<int> getProcStmts();
+
+    std::unordered_set<int> getWhileStmts();
+
+    std::unordered_set<int> getReadStmts();
+
+    std::unordered_set<int> getPrintStmts();
+
+    std::unordered_set<int> getCallStmts();
+
+    std::unordered_set<int> getIfStmts();
+
+    std::unordered_set<int> getAssignStmts();
+
+    std::unordered_set<std::string> getVars();
+
+    std::unordered_set<std::string> getConsts();
+
+private:
+    static std::unordered_set<std::string> procList;
+    static std::unordered_set<int> whileStmtList;
+    static std::unordered_set<int> readStmtList;
+    static std::unordered_set<int> printStmtList;
+    static std::unordered_set<int> callStmtList;
+    static std::unordered_set<int> ifStmtList; //need to create separate else-then or treat this as a block?
+    static std::unordered_set<int> assignStmtList;
+
+    static std::unordered_set<std::string> varList;
+    static std::unordered_set<std::string> constList;
+
+    //static FollowStore fStore;
+    //static ParentStore pStore;
+    static UsageStore usageStore;
+    static ModifyStore modifyStore;
 };
