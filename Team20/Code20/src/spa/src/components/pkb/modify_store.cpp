5cae8db37e85256150606da4c2ae26a79e20364f
Jaryl Loh <jarylloh1997@gmail.com>
2022-02-04 01:11:30 +0800

    Standardize indentations

--- Team20/Code20/src/spa/src/components/pkb/modify_store.cpp
+++ Team20/Code20/src/spa/src/components/pkb/modify_store.cpp
@@ -3,53 +3,53 @@
 ModifyStore::ModifyStore() {}
 
 bool ModifyStore::add_stmt_var(int stmt, std::string var) {
-    stmt_var_pairs.insert(std::make_pair(stmt, var));
+  stmt_var_pairs.insert(std::make_pair(stmt, var));
 }
 
 bool ModifyStore::add_proc_var(std::string proc, std::string var) {
-    proc_var_pairs.insert(std::make_pair(proc, var));
+  proc_var_pairs.insert(std::make_pair(proc, var));
 }
 
 bool ModifyStore::stmt_var_exists(std::pair<int, std::string> pair) {
-    return stmt_var_pairs.find(pair) != stmt_var_pairs.end();
+  return stmt_var_pairs.find(pair) != stmt_var_pairs.end();
 }
 
 bool ModifyStore::proc_var_exists(std::pair<std::string, std::string> pair) {
-    return proc_var_pairs.find(pair) != proc_var_pairs.end();
+  return proc_var_pairs.find(pair) != proc_var_pairs.end();
 }
 
 std::unordered_set<std::string> ModifyStore::get_var_mod_by_stmt(int stmt) {
-    if (stmt_var_map.find(stmt) != stmt_var_map.end()) {
-        return stmt_var_map.at(stmt);
-    }
-    return std::unordered_set<std::string>();
+  if (stmt_var_map.find(stmt) != stmt_var_map.end()) {
+    return stmt_var_map.at(stmt);
+  }
+  return std::unordered_set<std::string>();
 }
 
 std::unordered_set<int> ModifyStore::get_stmt_mod_by_var(std::string var) {
-    if (var_stmt_map.find(var) != var_stmt_map.end()) {
-        return var_stmt_map.at(var);
-    }
-    return std::unordered_set<int>();
+  if (var_stmt_map.find(var) != var_stmt_map.end()) {
+    return var_stmt_map.at(var);
+  }
+  return std::unordered_set<int>();
 }
 
 std::unordered_set<std::string> ModifyStore::get_var_mod_by_proc(std::string proc) {
-    if (proc_var_map.find(proc) != proc_var_map.end()) {
-        return proc_var_map.at(proc);
-    }
-    return std::unordered_set<std::string>();
+  if (proc_var_map.find(proc) != proc_var_map.end()) {
+    return proc_var_map.at(proc);
+  }
+  return std::unordered_set<std::string>();
 }
 
 std::unordered_set<std::string> ModifyStore::get_proc_mod_by_var(std::string var) {
-    if (var_proc_map.find(var) != var_proc_map.end()) {
-        return var_proc_map.at(var);
-    }
-    return std::unordered_set<std::string>();
+  if (var_proc_map.find(var) != var_proc_map.end()) {
+    return var_proc_map.at(var);
+  }
+  return std::unordered_set<std::string>();
 }
 
 std::unordered_set<std::pair<int, std::string>, hashFunction> ModifyStore::get_all_stmt_var() {
-    return stmt_var_pairs;
+  return stmt_var_pairs;
 }
 
 std::unordered_set<std::pair<std::string, std::string>, hashFunction> ModifyStore::get_all_proc_var() {
-    return proc_var_pairs;
+  return proc_var_pairs;
 }
--- Team20/Code20/src/spa/src/components/pkb/modify_store.h
+++ Team20/Code20/src/spa/src/components/pkb/modify_store.h
@@ -5,43 +5,43 @@
 #include <unordered_set>
 
 struct hashFunction {
-    size_t operator()(const std::pair<int , int> &x) const {
-        return x.first ^ x.second;
-    }
+  size_t operator()(const std::pair<int, int> &x) const {
+    return x.first ^ x.second;
+  }
 };
 
 /*
 	A store class that maintains all Modifies APIs and relationships
 */
 class ModifyStore {
-    public:
-        ModifyStore();
+ public:
+  ModifyStore();
 
-        bool add_stmt_var(int stmt, std::string var);
+  bool add_stmt_var(int stmt, std::string var);
 
-        bool add_proc_var(std::string proc, std::string var);
+  bool add_proc_var(std::string proc, std::string var);
 
-        bool stmt_var_exists(std::pair<int, std::string> pair);
+  bool stmt_var_exists(std::pair<int, std::string> pair);
 
-        bool proc_var_exists(std::pair<std::string, std::string> pair);
+  bool proc_var_exists(std::pair<std::string, std::string> pair);
 
-        std::unordered_set<std::string> get_var_mod_by_stmt(int stmt);
+  std::unordered_set<std::string> get_var_mod_by_stmt(int stmt);
 
-        std::unordered_set<int> get_stmt_mod_by_var(std::string var);
+  std::unordered_set<int> get_stmt_mod_by_var(std::string var);
 
-        std::unordered_set<std::string> get_var_mod_by_proc(std::string proc);
+  std::unordered_set<std::string> get_var_mod_by_proc(std::string proc);
 
-        std::unordered_set<std::string> get_proc_mod_by_var(std::string var);
+  std::unordered_set<std::string> get_proc_mod_by_var(std::string var);
 
-        std::unordered_set<std::pair<int, std::string>, hashFunction> get_all_stmt_var();
+  std::unordered_set<std::pair<int, std::string>, hashFunction> get_all_stmt_var();
 
-        std::unordered_set<std::pair<std::string, std::string>, hashFunction> get_all_proc_var();
+  std::unordered_set<std::pair<std::string, std::string>, hashFunction> get_all_proc_var();
 
-    private:
-        static std::unordered_set<std::pair<int, std::string>, hashFunction> stmt_var_pairs;
-        static std::unordered_set<std::pair<std::string, std::string>, hashFunction> proc_var_pairs;
-        static std::unordered_map<int, std::unordered_set<std::string> > stmt_var_map;
-        static std::unordered_map<std::string, std::unordered_set<int> > var_stmt_map;
-        static std::unordered_map<std::string, std::unordered_set<std::string> > proc_var_map;
-        static std::unordered_map<std::string, std::unordered_set<std::string> > var_proc_map;
+ private:
+  static std::unordered_set<std::pair<int, std::string>, hashFunction> stmt_var_pairs;
+  static std::unordered_set<std::pair<std::string, std::string>, hashFunction> proc_var_pairs;
+  static std::unordered_map<int, std::unordered_set<std::string> > stmt_var_map;
+  static std::unordered_map<std::string, std::unordered_set<int> > var_stmt_map;
+  static std::unordered_map<std::string, std::unordered_set<std::string> > proc_var_map;
+  static std::unordered_map<std::string, std::unordered_set<std::string> > var_proc_map;
 };
