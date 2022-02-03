3da155689d99640be2bdc1eabda9a2481daadc9a
Jaryl Loh <jarylloh1997@gmail.com>
2022-02-03 15:58:37 +0800

    feat: Implement Parent/Parent* APIs

--- Team20/Code20/src/spa/src/components/pkb/ParentStore.cpp
+++ Team20/Code20/src/spa/src/components/pkb/ParentStore.cpp
@@ -3,45 +3,77 @@
 ParentStore::ParentStore() {}
 
 bool ParentStore::isParent(int stmt) {
-
+    return parentSet.find(stmt) != parentSet.end();
 }
 
 bool ParentStore::isChild(int stmt) {
-
+    return childSet.find(stmt) != childSet.end();
 }
 
 bool ParentStore::isAnce(int stmt) {
-
-} // For Parent*
+    return anceSet.find(stmt) != anceSet.end();
+}
 
 bool ParentStore::isDesc(int stmt) {
+    return descSet.find(stmt) != descSet.end();
+}
 
-}// For Parent*
+void ParentStore::rsInit(int numStmts) {
+    parentChild newPC = {0, 0, std::unordered_set<int>(), std::unordered_set<int>()};
 
-bool ParentStore::addParentStmt(int parent, int child) {
+    for (int i = 1; i <= numStmts; i++) {
+        rsMap[i] = newPC;
+    }
+}
 
+void ParentStore::addParentStmt(int parent, int child) {
+    if (!parentChildExists(parent, child)) {
+        parentChildSet.insert(std::make_pair(parent, child));
+        rsMap.at(parent).child = child;
+        rsMap.at(child).parent = parent;
+    }
+
+    rsMap.at(parent).desc.insert(child);
+    rsMap.at(child).ance.insert(parent);
 }
 
 bool ParentStore::parentChildExists(int stmt1, int stmt2) {
-
+    std::pair<int, int> p = std::make_pair(stmt1, stmt2);
+    return parentChildSet.find(p) != parentChildSet.end();
 }
 
 int ParentStore::getParent(int stmt) {
-
+    if (rsMap.find(stmt) != rsMap.end()) {
+        parentChild pc = rsMap.at(stmt);
+        return pc.parent;
+    }
+    return 0;
 }
 
 int ParentStore::getChild(int stmt) {
-
+    if (rsMap.find(stmt) != rsMap.end()) {
+        parentChild pc = rsMap.at(stmt);
+        return pc.child;
+    }
+    return 0;
 }
 
-int ParentStore::getAllAnce(int stmt) {
-
+std::unordered_set<int> ParentStore::getAllAnce(int stmt) {
+    if (rsMap.find(stmt) != rsMap.end()) {
+        parentChild pc = rsMap.at(stmt);
+        return pc.ance;
+    }
+    return std::unordered_set<int>();
 }
 
-int ParentStore::getAllDesc(int stmt) {
-
+std::unordered_set<int> ParentStore::getAllDesc(int stmt) {
+    if (rsMap.find(stmt) != rsMap.end()) {
+        parentChild pc = rsMap.at(stmt);
+        return pc.desc;
+    }
+    return std::unordered_set<int>();
 }
 
 std::unordered_set< std::pair<int, int>, hashFunction> ParentStore::getParentChildPairs() {
-
+    return parentChildSet;
 }
--- Team20/Code20/src/spa/src/components/pkb/ParentStore.h
+++ Team20/Code20/src/spa/src/components/pkb/ParentStore.h
@@ -11,8 +11,7 @@ struct hashFunction {
 };
 
 // A struct to maintain every node's relationships
-struct parentChild
-{
+struct parentChild {
     int parent;
     int child;
     std::unordered_set<int> ance;
@@ -30,29 +29,31 @@ public:
 
     bool isChild(int stmt);
 
-    bool isAnce(int stmt); // For Parent*
+    bool isAnce(int stmt);
 
-    bool isDesc(int stmt); // For Parent*
-
-    bool addParentStmt(int parent, int child);
+    bool isDesc(int stmt);
 
     bool parentChildExists(int stmt1, int stmt2);
+
+    void rsInit(int numStmts);
+
+    void addParentStmt(int parent, int child);
 
     int getParent(int stmt);
 
     int getChild(int stmt);
 
-    int getAllAnce(int stmt);
+    std::unordered_set<int> getAllAnce(int stmt);
 
-    int getAllDesc(int stmt);
+    std::unordered_set<int> getAllDesc(int stmt);
 
     std::unordered_set< std::pair<int, int>, hashFunction> getParentChildPairs();
 
 private:
-    static std::unordered_map<int, parentChild> relationshipMap;
+    static std::unordered_map<int, parentChild> rsMap;
     static std::unordered_set< std::pair<int, int>, hashFunction> parentChildSet;
-    static std::unordered_map<int, std::unordered_set<int> > anceDescMap;
-    static std::unordered_map<int, std::unordered_set<int> > descAnceMap;
+    static std::unordered_set<int> anceSet;
+    static std::unordered_set<int> descSet;
     static std::unordered_set<int> parentSet;
     static std::unordered_set<int> childSet;
 };
