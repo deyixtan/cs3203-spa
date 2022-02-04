0d5f339900a325b6ccc5f3da1cc80ee807d04351
YuHeem <54734556+yuheem@users.noreply.github.com>
2022-02-04 23:33:19 +0800

    Update pql pattern

--- Team20/Code20/src/spa/src/components/query_subsystem/pql_parser/pattern.cpp
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_parser/pattern.cpp
@@ -1,1 +1,18 @@
 #include "pattern.h"
+
+Pattern::Pattern(PqlToken syn_assign, PqlToken first, PqlToken second)
+    : syn_assign_(syn_assign),
+      first_(first),
+      second_(second) {}
+
+PqlToken Pattern::GetSynAssign() {
+  return syn_assign_;
+}
+
+PqlToken Pattern::GetFirst() {
+  return first_;
+}
+
+PqlToken Pattern::GetSecond() {
+  return second_;
+}
