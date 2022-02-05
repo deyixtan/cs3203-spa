40f5d4d69ca9f97eec350e34eee0ff72f6134d6b
ddanelynn <dingyoujia0823@gmail.com>
2022-02-05 15:38:57 +0800

    complete validation for select

--- Team20/Code20/src/spa/src/components/query_subsystem/pql_parser/query_validator.cpp
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_parser/query_validator.cpp
@@ -48,11 +48,99 @@ bool QueryValidator::IsValidRelRefClause(std::vector<PqlToken> rel_ref_tokens) {
   return true;
 }
 
-bool QueryValidator::IsValidRelRefArg(std::vector<PqlToken> rel_ref_tokens, TokenType type) {
-  if (type == TokenType::USES) {
-
+void QueryValidator::IsValidRelRefArg(std::vector<PqlToken> rel_ref_tokens, TokenType type) {
+  PqlToken first_arg = rel_ref_tokens[2];
+  PqlToken second_arg = rel_ref_tokens[4];
+  if (type == TokenType::USES || type == TokenType::MODIFIES) {
+    if (!ent_ref.count(second_arg.type)) {
+      throw "ERROR: Invalid arguments for rel ref! \n";
+    }
+  } else if (type == TokenType::PARENT || type == TokenType::PARENT_T ||
+             type == TokenType::FOLLOWS || type == TokenType::FOLLOWS_T) {
+    if (!stmt_ref.count(first_arg.type) || !stmt_ref.count(second_arg.type)) {
+      throw "ERROR: Invalid arguments for rel ref! \n";
+    }
+  } else {
+    throw "ERROR: Invalid rel ref token! \n";
   }
 }
+
+void QueryValidator::IsValidDeclaration(std::vector<PqlToken> declaration_query) {
+  if (declaration_query.size() < 3) {
+    throw "ERROR: Missing token in declaration! \n";
+  }
+  PqlToken design_entities_token = declaration_query[0];
+  if (!IsValidDesignEntity(design_entities_token)) {
+    throw "ERROR: Invalid Design Entity! \n";
+  }
+  for (int j = 1; j < declaration_query.size() - 1; j++) {
+    if (j % 2 == 0) {
+      if (declaration_query[j].type != TokenType::COMMA) {
+        throw "ERROR: Invalid Declaration format! \n";
+      }
+    } else {
+      if (!IsValidSynonym(declaration_query[j]) {
+        throw "ERROR: Invalid Declaration format! \n";
+      }
+    }
+  }
+  PqlToken semicolon_token = declaration_query[declaration_query.size() - 1];
+  if (semicolon_token.type != TokenType::SEMICOLON) {
+    throw "Invalid Declaration format! \n";
+  }
+}
+
+void QueryValidator::IsValidPatternClause(std::vector<PqlToken> pattern_clause) {
+  if (pattern_clause.size() != 7) {
+    throw "Invalid Pattern Clause format! \n";
+  }
+}
+
+void QueryValidator::IsValidSelectClause(std::vector<PqlToken> select_clause) {
+  if (select_clause.size() < 2) {
+    throw "Invalid Select Clause format! \n";
+  }
+  PqlToken select_token = select_clause[0];
+  if (select_token.type != TokenType::SELECT) {
+    throw "Invalid Select keyword! \n";
+  }
+  // Select v;
+
+  PqlToken synonym_token = select_clause[1];
+  if (!IsValidSynonym(synonym_token)) {
+    throw "Invalid synonym to be selected! \n";
+  }
+
+  if (select_clause.size() >= 2) { // Select s such that Follows (s, s1)
+    PqlToken such_token = select_clause[2];
+    if (such_token.type != TokenType::SUCH) {
+      throw "Invalid Select Clause format! \n";
+    }
+    PqlToken that_token = select_clause[3];
+    if (that_token.type != TokenType::THAT) {
+      throw "Invalid Select Clause format! \n";
+    }
+
+    std::vector<PqlToken> rel_ref_tokens;
+    for (int index = 4; index < 10; index++) {
+      rel_ref_tokens.push_back(select_clause[index]);
+    }
+
+    if (IsValidRelRefClause(rel_ref_tokens)) {
+      IsValidRelRefArg(rel_ref_tokens, rel_ref_tokens[0].type);
+    } else {
+      throw "ERROR: Invalid rel ref format! \n";
+    }
+  }
+
+  if (select_clause.size() >= 10) { // Select s such that Follows (s, s1) pattern a (a, "x")
+    std::vector<PqlToken> pattern_tokens;
+    for (int index = 10; index < select_clause.size() - 1; index++) {
+      pattern_tokens.push_back(select_clause[index]);
+    }
+  }
+}
+
 
 std::vector<PqlToken> QueryValidator::CheckValidation() {
   std::vector<std::vector<PqlToken>> token_table;
@@ -83,49 +171,12 @@ std::vector<PqlToken> QueryValidator::CheckValidation() {
   // check declaration query (excluding the last vector in the table)
   for (int i = 0; i < last_declaration_index + 1; i++) {
     std::vector<PqlToken> declaration_query = token_table[i];
-    if (declaration_query.size() < 3) {
-      throw "ERROR: Missing token in declaration! \n";
-    }
-    PqlToken design_entities_token = declaration_query[0];
-    if (!IsValidDesignEntity(design_entities_token)) {
-      throw "ERROR: Invalid Design Entity! \n";
-    }
-    for (int j = 1; j < declaration_query.size() - 1; j++) {
-      if (j % 2 == 0) {
-        if (declaration_query[j].type != TokenType::COMMA) {
-          throw "ERROR: Invalid Declaration format! \n";
-        }
-      } else {
-        if (!IsValidSynonym(declaration_query[j]) {
-          throw "ERROR: Invalid Declaration format! \n";
-        }
-      }
-    }
-    PqlToken semicolon_token = declaration_query[declaration_query.size() - 1];
-    if (semicolon_token.type != TokenType::SEMICOLON) {
-      throw "Invalid Declaration format! \n";
-    }
+    IsValidDeclaration(declaration_query);
+  }
 
-    if (contain_select_clause) {
-      std::vector<PqlToken> select_clause = token_table[token_table.size() - 1];
-      if (select_clause.size() < 2) {
-        throw "Invalid Select Clause format! \n";
-      }
-      PqlToken select_token = select_clause[0];
-      if (select_token.type != TokenType::SELECT) {
-        throw "Invalid Select keyword! \n";
-      }
-      // Select v;
-      if (select_clause.size() == 2) {
-        PqlToken synonym_token = select_clause[1];
-        if (!IsValidSynonym(synonym_token)) {
-          throw "Invalid synonym to be selected! \n";
-        }
-      } else {
-
-      }
-    }
-
+  if (contain_select_clause) {
+    std::vector<PqlToken> select_clause = token_table[token_table.size() - 1];
+    IsValidSelectClause(select_clause);
   }
 
   return tokens_;
