d006678dc2d4898d1d2c290b3dc4a5d8939dc56e
Deyi Tan <deyixtan@gmail.com>
2022-02-11 18:27:23 +0800

    Refactor ProgramNode class

--- Team20/Code20/src/spa/src/components/source_subsystem/ast/node_program.cpp
+++ Team20/Code20/src/spa/src/components/source_subsystem/ast/node_program.cpp
@@ -3,18 +3,18 @@
 ProgramNode::ProgramNode(std::vector<std::shared_ptr<ProcedureNode>> procedures) :
     m_procedures(procedures) {}
 
-std::vector<std::shared_ptr<ProcedureNode>> ProgramNode::getProcedures() {
+std::vector<std::shared_ptr<ProcedureNode>> ProgramNode::GetProcedures() {
   return m_procedures;
+}
+
+std::string ProgramNode::ToString(int level) {
+  std::string str = "";
+  for (std::shared_ptr<ProcedureNode> procedure : m_procedures) {
+    str += procedure->ToString(level) + "\n";
+  }
+  return str;
 }
 
 std::string ProgramNode::ToString() {
   return this->ToString(0);
-}
-
-std::string ProgramNode::ToString(int level) {
-  std::string acc = "";
-  for (std::shared_ptr<ProcedureNode> procedure : m_procedures) {
-    acc += procedure->ToString(level) + "\n";
-  }
-  return acc;
 }
