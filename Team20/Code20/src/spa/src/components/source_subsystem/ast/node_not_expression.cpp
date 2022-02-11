5ba6846f8df8e5b1ad9aa82ae5839d1b5065eff8
Deyi Tan <deyixtan@gmail.com>
2022-02-11 19:58:21 +0800

    Refactor NotExpressionNode class

--- Team20/Code20/src/spa/src/components/source_subsystem/ast/node_not_expression.cpp
+++ Team20/Code20/src/spa/src/components/source_subsystem/ast/node_not_expression.cpp
@@ -1,14 +1,16 @@
 #include "node_not_expression.h"
-NotExpressionNode::NotExpressionNode(std::shared_ptr<ConditionalExpressionNode> expr) : m_expr(expr) {}
 
-std::shared_ptr<ConditionalExpressionNode> NotExpressionNode::getExpr() {
-  return m_expr;
+NotExpressionNode::NotExpressionNode(std::shared_ptr<ConditionalExpressionNode> expression)
+    : m_expression(expression) {}
+
+std::shared_ptr<ConditionalExpressionNode> NotExpressionNode::GetExpression() {
+  return m_expression;
 }
 
-ConditionalType NotExpressionNode::getConditionalType() {
+ConditionalType NotExpressionNode::GetConditionalType() {
   return ConditionalType::NOT;
 }
 
 std::string NotExpressionNode::ToString(int level) {
-  return "(!" + m_expr->ToString(level) + ")";
+  return "(!" + m_expression->ToString(level) + ")";
 }
--- Team20/Code20/src/spa/src/components/source_subsystem/ast/node_not_expression.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/ast/node_not_expression.h
@@ -1,17 +1,17 @@
-#ifndef SPA_SRC_SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_NOT_EXPRESSION_H_
-#define SPA_SRC_SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_NOT_EXPRESSION_H_
+#ifndef SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_NOT_EXPRESSION_H_
+#define SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_NOT_EXPRESSION_H_
 
 #include "node_conditional_expression.h"
 
 class NotExpressionNode : public ConditionalExpressionNode {
  private:
-  std::shared_ptr<ConditionalExpressionNode> m_expr;
+  std::shared_ptr<ConditionalExpressionNode> m_expression;
 
  public:
-  NotExpressionNode(std::shared_ptr<ConditionalExpressionNode> expr);
-  std::shared_ptr<ConditionalExpressionNode> getExpr();
-  ConditionalType getConditionalType();
-  std::string ToString(int level);
+  NotExpressionNode(std::shared_ptr<ConditionalExpressionNode> expression);
+  [[nodiscard]] std::shared_ptr<ConditionalExpressionNode> GetExpression();
+  [[nodiscard]] ConditionalType GetConditionalType() override;
+  [[nodiscard]] std::string ToString(int level) override;
 };
 
-#endif //SPA_SRC_SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_NOT_EXPRESSION_H_
+#endif //SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_NOT_EXPRESSION_H_
