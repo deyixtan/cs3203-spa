79bf939a4547a3db398493a0f38d7e2510ed9260
Deyi Tan <deyixtan@gmail.com>
2022-02-11 18:22:12 +0800

    Refactor PrintStatementNode class

--- Team20/Code20/src/spa/src/components/source_subsystem/ast/node_print_statement.cpp
+++ Team20/Code20/src/spa/src/components/source_subsystem/ast/node_print_statement.cpp
@@ -1,15 +1,16 @@
 #include "node_print_statement.h"
 
-PrintStatementNode::PrintStatementNode(int line, std::shared_ptr<VariableNode> id) : StatementNode(line), m_id(id) {}
+PrintStatementNode::PrintStatementNode(int stmt_no, std::shared_ptr<VariableNode> identifier)
+    : StatementNode(stmt_no), m_identifier(identifier) {}
 
-std::shared_ptr<VariableNode> PrintStatementNode::getId() {
-  return m_id;
+std::shared_ptr<VariableNode> PrintStatementNode::GetIdentifier() {
+  return m_identifier;
 }
 
-StmtType PrintStatementNode::getStatementType() {
+StmtType PrintStatementNode::GetStatementType() {
   return StmtType::PRINT;
 }
 
 std::string PrintStatementNode::ToString(int level) {
-  return StatementNode::ToString(level) + "print " + m_id->ToString(level) + ";\n";
+  return StatementNode::ToString(level) + "print " + m_identifier->ToString(level) + ";\n";
 }
