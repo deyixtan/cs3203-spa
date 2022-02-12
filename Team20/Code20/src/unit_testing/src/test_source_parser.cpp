398fd4d1f2d6e4f3a662965de6e7546385c356c1
Deyi Tan <deyixtan@gmail.com>
2022-02-13 03:15:01 +0800

    Add simple read and print statement test cases for SourceParser

--- Team20/Code20/src/unit_testing/src/test_source_parser.cpp
+++ Team20/Code20/src/unit_testing/src/test_source_parser.cpp
@@ -33,3 +33,59 @@ TEST_CASE("Test single procedure with one assign statement") {
   // TODO: overload equality operator for all Node classes
   REQUIRE(program_node == expected_program_node);
 }
+
+TEST_CASE("Test single procedure with one read statement") {
+  // actual
+  std::vector<std::shared_ptr<SourceToken>> token_list;
+  std::shared_ptr<ProgramNode> program_node;
+
+  SourceLexer lexer = SourceLexer("procedure main { read a; }");
+  lexer.Tokenize(token_list);
+  SourceParser parser = SourceParser(token_list);
+  program_node = parser.ParseProgram();
+
+  // expected
+  std::shared_ptr<VariableNode> variable_node = std::make_shared<VariableNode>("a");
+  std::shared_ptr<ReadStatementNode>
+      assign_stmt = std::make_shared<ReadStatementNode>(1, variable_node);
+
+  std::vector<std::shared_ptr<StatementNode>> statements;
+  statements.emplace_back(assign_stmt);
+  std::shared_ptr<StatementListNode> stmt_list = std::make_shared<StatementListNode>(statements);
+
+  std::shared_ptr<ProcedureNode> procedure = std::make_shared<ProcedureNode>("main", stmt_list);
+
+  std::vector<std::shared_ptr<ProcedureNode>> procedures;
+  procedures.emplace_back(procedure);
+  std::shared_ptr<ProgramNode> expected_program_node = std::make_shared<ProgramNode>(procedures);
+
+  REQUIRE(program_node == expected_program_node);
+}
+
+TEST_CASE("Test single procedure with one print statement") {
+  // actual
+  std::vector<std::shared_ptr<SourceToken>> token_list;
+  std::shared_ptr<ProgramNode> program_node;
+
+  SourceLexer lexer = SourceLexer("procedure main { print a; }");
+  lexer.Tokenize(token_list);
+  SourceParser parser = SourceParser(token_list);
+  program_node = parser.ParseProgram();
+
+  // expected
+  std::shared_ptr<VariableNode> variable_node = std::make_shared<VariableNode>("a");
+  std::shared_ptr<PrintStatementNode>
+      assign_stmt = std::make_shared<PrintStatementNode>(1, variable_node);
+
+  std::vector<std::shared_ptr<StatementNode>> statements;
+  statements.emplace_back(assign_stmt);
+  std::shared_ptr<StatementListNode> stmt_list = std::make_shared<StatementListNode>(statements);
+
+  std::shared_ptr<ProcedureNode> procedure = std::make_shared<ProcedureNode>("main", stmt_list);
+
+  std::vector<std::shared_ptr<ProcedureNode>> procedures;
+  procedures.emplace_back(procedure);
+  std::shared_ptr<ProgramNode> expected_program_node = std::make_shared<ProgramNode>(procedures);
+
+  REQUIRE(program_node == expected_program_node);
+}
