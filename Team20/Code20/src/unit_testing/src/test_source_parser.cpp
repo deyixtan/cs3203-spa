1671534473cba0f7510629bb7094e7a0265d5cf3
Deyi Tan <deyixtan@gmail.com>
2022-02-13 20:21:30 +0800

    Add tests involving statements not being enclosed in a procedure

--- Team20/Code20/src/unit_testing/src/test_source_parser.cpp
+++ Team20/Code20/src/unit_testing/src/test_source_parser.cpp
@@ -1,37 +1,73 @@
 #include "catch.hpp"
 #include "components/source_subsystem/source_lexer.h"
 #include "components/source_subsystem/source_parser.h"
+#include "components/source_subsystem/exceptions/mismatch_token.h"
 
 using namespace source;
 
-TEST_CASE("Test single procedure with one assign statement") {
+TEST_CASE("Test one read statement") {
   // set up actual
   std::vector<std::shared_ptr<SourceToken>> token_list;
   std::shared_ptr<ProgramNode> program_node;
 
-  SourceLexer lexer = SourceLexer("procedure main { a = 1; }");
+  SourceLexer lexer = SourceLexer("read x;");
   lexer.Tokenize(token_list);
   SourceParser parser = SourceParser(token_list);
-  program_node = parser.ParseProgram();
-
-  // set up expected
-  std::shared_ptr<VariableNode> variable_node = std::make_shared<VariableNode>("a");
-  std::shared_ptr<ConstantNode> constant_node = std::make_shared<ConstantNode>("1");
-  std::shared_ptr<AssignStatementNode>
-      assign_stmt = std::make_shared<AssignStatementNode>(1, variable_node, constant_node);
-
-  std::vector<std::shared_ptr<StatementNode>> statements;
-  statements.emplace_back(assign_stmt);
-  std::shared_ptr<StatementListNode> stmt_list = std::make_shared<StatementListNode>(statements);
-
-  std::shared_ptr<ProcedureNode> procedure = std::make_shared<ProcedureNode>("main", stmt_list);
-
-  std::vector<std::shared_ptr<ProcedureNode>> procedures;
-  procedures.emplace_back(procedure);
-  std::shared_ptr<ProgramNode> expected_program_node = std::make_shared<ProgramNode>(procedures);
 
   // test
-  REQUIRE(*program_node == *expected_program_node);
+  REQUIRE_THROWS_WITH(parser.ParseProgram(), MismatchedTokenException().what());
+}
+
+TEST_CASE("Test one print statement") {
+  // set up actual
+  std::vector<std::shared_ptr<SourceToken>> token_list;
+  std::shared_ptr<ProgramNode> program_node;
+
+  SourceLexer lexer = SourceLexer("print x;");
+  lexer.Tokenize(token_list);
+  SourceParser parser = SourceParser(token_list);
+
+  // test
+  REQUIRE_THROWS_WITH(parser.ParseProgram(), MismatchedTokenException().what());
+}
+
+TEST_CASE("Test one if-statement") {
+  // set up actual
+  std::vector<std::shared_ptr<SourceToken>> token_list;
+  std::shared_ptr<ProgramNode> program_node;
+
+  SourceLexer lexer = SourceLexer("if (a == 1) then { a = 2; } else { a = 3; }");
+  lexer.Tokenize(token_list);
+  SourceParser parser = SourceParser(token_list);
+
+  // test
+  REQUIRE_THROWS_WITH(parser.ParseProgram(), MismatchedTokenException().what());
+}
+
+TEST_CASE("Test one while-statement") {
+  // set up actual
+  std::vector<std::shared_ptr<SourceToken>> token_list;
+  std::shared_ptr<ProgramNode> program_node;
+
+  SourceLexer lexer = SourceLexer("while (a == 1) { a = 2; }");
+  lexer.Tokenize(token_list);
+  SourceParser parser = SourceParser(token_list);
+
+  // test
+  REQUIRE_THROWS_WITH(parser.ParseProgram(), MismatchedTokenException().what());
+}
+
+TEST_CASE("Test one assign statement") {
+  // set up actual
+  std::vector<std::shared_ptr<SourceToken>> token_list;
+  std::shared_ptr<ProgramNode> program_node;
+
+  SourceLexer lexer = SourceLexer("a = 1;");
+  lexer.Tokenize(token_list);
+  SourceParser parser = SourceParser(token_list);
+
+  // test
+  REQUIRE_THROWS_WITH(parser.ParseProgram(), MismatchedTokenException().what());
 }
 
 TEST_CASE("Test single procedure with one read statement") {
@@ -184,6 +220,36 @@ TEST_CASE("Test single procedure with one while statement (simple equal conditio
   // while_stmt to procedure's stmt_list
   std::vector<std::shared_ptr<StatementNode>> statements;
   statements.emplace_back(while_stmt);
+  std::shared_ptr<StatementListNode> stmt_list = std::make_shared<StatementListNode>(statements);
+
+  std::shared_ptr<ProcedureNode> procedure = std::make_shared<ProcedureNode>("main", stmt_list);
+
+  std::vector<std::shared_ptr<ProcedureNode>> procedures;
+  procedures.emplace_back(procedure);
+  std::shared_ptr<ProgramNode> expected_program_node = std::make_shared<ProgramNode>(procedures);
+
+  // test
+  REQUIRE(*program_node == *expected_program_node);
+}
+
+TEST_CASE("Test single procedure with one assign statement") {
+  // set up actual
+  std::vector<std::shared_ptr<SourceToken>> token_list;
+  std::shared_ptr<ProgramNode> program_node;
+
+  SourceLexer lexer = SourceLexer("procedure main { a = 1; }");
+  lexer.Tokenize(token_list);
+  SourceParser parser = SourceParser(token_list);
+  program_node = parser.ParseProgram();
+
+  // set up expected
+  std::shared_ptr<VariableNode> variable_node = std::make_shared<VariableNode>("a");
+  std::shared_ptr<ConstantNode> constant_node = std::make_shared<ConstantNode>("1");
+  std::shared_ptr<AssignStatementNode>
+      assign_stmt = std::make_shared<AssignStatementNode>(1, variable_node, constant_node);
+
+  std::vector<std::shared_ptr<StatementNode>> statements;
+  statements.emplace_back(assign_stmt);
   std::shared_ptr<StatementListNode> stmt_list = std::make_shared<StatementListNode>(statements);
 
   std::shared_ptr<ProcedureNode> procedure = std::make_shared<ProcedureNode>("main", stmt_list);
