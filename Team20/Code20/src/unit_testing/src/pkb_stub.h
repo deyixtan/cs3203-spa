852e1ef3a438252afa2f6454af0506bff3c55abc
Jaryl Loh <jarylloh1997@gmail.com>
2022-02-14 03:11:33 +0800

    Add unit tests for pkb

--- Team20/Code20/src/unit_testing/src/pkb_stub.h
+++ Team20/Code20/src/unit_testing/src/pkb_stub.h
@@ -2,8 +2,79 @@
 #define PKB_STUB_H
 #include "components/pkb/pkb.h"
 
-std::unordered_set<std::string> stmt_list = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-std::unordered_set<std::string> var_list = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-std::unordered_set<std::string> assign_stmt_list = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+std::unordered_set<std::string> proc_list = {"main", "foo", "bar", "funcX", "funcY", "funcZ"};
+std::unordered_set<std::string> stmt_list = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"};
+std::unordered_set<std::string> var_list = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"};
+std::unordered_set<std::string> assign_list = {"1", "2", "3"};
+std::unordered_set<std::string> if_list = {"4", "5", "6"};
+std::unordered_set<std::string> while_list = {"7"};
+std::unordered_set<std::string> const_list = {"1", "4", "7", "9", "11"};
+std::unordered_set<std::string> read_list = {"8", "9", "10", "11"};
+std::unordered_set<std::string> print_list = {"12", "13", "14", "15"};
+
+std::unordered_map<std::string, std::unordered_set<std::string>> proc_to_stmt =
+    {
+      {"main", {"1", "2", "3", "4", "5", "6"}},
+      {"foo", {"7"}},
+      {"bar", {"8", "9"}},
+      {"funcX", {"10", "11", "12"}},
+      {"funcY", {"13", "14"}},
+      {"funcZ", {"15"}}
+    };
+
+std::unordered_map<std::string, std::unordered_set<std::string>> stmt_to_proc =
+    {
+        {"main", {"1", "2", "3", "4", "5", "6"}},
+        {"foo", {"7"}},
+        {"bar", {"8", "9"}},
+        {"funcX", {"10", "11", "12"}},
+        {"funcY", {"13", "14"}},
+        {"funcZ", {"15"}}
+    };
+
+std::unordered_map<std::string, std::unordered_set<std::string>> stmt_to_var =
+    {
+        {"1", {"a", "b", "f", "i"}},
+        {"2", {"c"}},
+        {"3", {}},
+        {"4", {"d", "k", "n", "s"}},
+        {"5", {"e", "o"}},
+        {"6", {"a", "f", "g", "h", "i", "j"}},
+        {"7", {"c", "k", "l"}},
+        {"8", {"m", "p"}},
+        {"9", {"c", "n"}},
+        {"10", {"o"}},
+        {"11", {"c", "p", "q"}},
+        {"12", {"r"}},
+        {"13", {}},
+        {"14", {"s", "t", "u"}},
+        {"15", {"v"}},
+    };
+
+std::unordered_map<std::string, std::unordered_set<std::string>> var_to_stmt =
+    {
+        {"a", {"1", "6"}},
+        {"b", {"1"}},
+        {"c", {"2", "7", "9", "11"}},
+        {"d", {"4"}},
+        {"e", {"5"}},
+        {"f", {"1", "6"}},
+        {"g", {"6"}},
+        {"h", {"6"}},
+        {"i", {"1", "6"}},
+        {"j", {"6"}},
+        {"k", {"4", "7"}},
+        {"l", {"7"}},
+        {"m", {"8"}},
+        {"n", {"4", "9"}},
+        {"o", {"5", "10"}},
+        {"p", {"8", "11"}},
+        {"q", {"11"}},
+        {"r", {"12"}},
+        {"s", {"4", "14"}},
+        {"t", {"14"}},
+        {"u", {"14"}},
+        {"v", {"15"}},
+    };
 
 #endif //PKB_STUB_H
--- Team20/Code20/src/unit_testing/src/pkb_test.cpp
+++ Team20/Code20/src/unit_testing/src/pkb_test.cpp
@@ -4,6 +4,19 @@
 
 void set_up_pkb() {
   PKB *pkb = PKB::GetInstance();
+  pkb->AddStmt("main", PROC);
+  pkb->AddStmt("foo", PROC);
+  pkb->AddStmt("bar", PROC);
+  pkb->AddStmt("funcX", PROC);
+  pkb->AddStmt("funcY", PROC);
+  pkb->AddStmt("funcZ", PROC);
+
+  pkb->AddStmt("4", IF);
+  pkb->AddStmt("5", IF);
+  pkb->AddStmt("6", IF);
+
+  pkb->AddStmt("7", WHILE);
+
   pkb->AddStmt("a", VARS);
   pkb->AddStmt("b", VARS);
   pkb->AddStmt("c", VARS);
@@ -14,6 +27,18 @@ void set_up_pkb() {
   pkb->AddStmt("h", VARS);
   pkb->AddStmt("i", VARS);
   pkb->AddStmt("j", VARS);
+  pkb->AddStmt("k", VARS);
+  pkb->AddStmt("l", VARS);
+  pkb->AddStmt("m", VARS);
+  pkb->AddStmt("n", VARS);
+  pkb->AddStmt("o", VARS);
+  pkb->AddStmt("p", VARS);
+  pkb->AddStmt("q", VARS);
+  pkb->AddStmt("r", VARS);
+  pkb->AddStmt("s", VARS);
+  pkb->AddStmt("t", VARS);
+  pkb->AddStmt("u", VARS);
+  pkb->AddStmt("v", VARS);
 
   pkb->AddStmt("1", STMT);
   pkb->AddStmt("2", STMT);
@@ -25,17 +50,82 @@ void set_up_pkb() {
   pkb->AddStmt("8", STMT);
   pkb->AddStmt("9", STMT);
   pkb->AddStmt("10", STMT);
+  pkb->AddStmt("11", STMT);
+  pkb->AddStmt("12", STMT);
+  pkb->AddStmt("13", STMT);
+  pkb->AddStmt("14", STMT);
+  pkb->AddStmt("15", STMT);
 
   pkb->AddStmt("1", ASSIGN);
   pkb->AddStmt("2", ASSIGN);
   pkb->AddStmt("3", ASSIGN);
-  pkb->AddStmt("4", ASSIGN);
-  pkb->AddStmt("5", ASSIGN);
-  pkb->AddStmt("6", ASSIGN);
-  pkb->AddStmt("7", ASSIGN);
-  pkb->AddStmt("8", ASSIGN);
-  pkb->AddStmt("9", ASSIGN);
-  pkb->AddStmt("10", ASSIGN);
+
+  pkb->AddStmt("1", CONSTS);
+  pkb->AddStmt("4", CONSTS);
+  pkb->AddStmt("7", CONSTS);
+  pkb->AddStmt("9", CONSTS);
+  pkb->AddStmt("11", CONSTS);
+
+  pkb->AddStmt("8", READ);
+  pkb->AddStmt("9", READ);
+  pkb->AddStmt("10", READ);
+  pkb->AddStmt("11", READ);
+
+  pkb->AddStmt("12", PRINT);
+  pkb->AddStmt("13", PRINT);
+  pkb->AddStmt("14", PRINT);
+  pkb->AddStmt("15", PRINT);
+
+  pkb->AddUsageProcVar("main", "1");
+  pkb->AddUsageProcVar("main", "2");
+  pkb->AddUsageProcVar("main", "3");
+  pkb->AddUsageProcVar("main", "4");
+  pkb->AddUsageProcVar("main", "5");
+  pkb->AddUsageProcVar("main", "6");
+  pkb->AddUsageProcVar("foo", "7");
+  pkb->AddUsageProcVar("bar", "8");
+  pkb->AddUsageProcVar("bar", "9");
+  pkb->AddUsageProcVar("funcX", "10");
+  pkb->AddUsageProcVar("funcX", "11");
+  pkb->AddUsageProcVar("funcX", "12");
+  pkb->AddUsageProcVar("funcY", "13");
+  pkb->AddUsageProcVar("funcY", "14");
+  pkb->AddUsageProcVar("funcZ", "15");
+
+  pkb->AddUsageStmtVar("1", "a");
+  pkb->AddUsageStmtVar("1", "b");
+  pkb->AddUsageStmtVar("1", "f");
+  pkb->AddUsageStmtVar("1", "i");
+  pkb->AddUsageStmtVar("2", "c");
+  pkb->AddUsageStmtVar("4", "d");
+  pkb->AddUsageStmtVar("4", "k");
+  pkb->AddUsageStmtVar("4", "n");
+  pkb->AddUsageStmtVar("4", "s");
+  pkb->AddUsageStmtVar("5", "e");
+  pkb->AddUsageStmtVar("5", "o");
+  pkb->AddUsageStmtVar("6", "a");
+  pkb->AddUsageStmtVar("6", "f");
+  pkb->AddUsageStmtVar("6", "g");
+  pkb->AddUsageStmtVar("6", "h");
+  pkb->AddUsageStmtVar("6", "i");
+  pkb->AddUsageStmtVar("6", "j");
+  pkb->AddUsageStmtVar("7", "c");
+  pkb->AddUsageStmtVar("7", "k");
+  pkb->AddUsageStmtVar("7", "l");
+  pkb->AddUsageStmtVar("8", "m");
+  pkb->AddUsageStmtVar("8", "p");
+  pkb->AddUsageStmtVar("9", "c");
+  pkb->AddUsageStmtVar("9", "n");
+  pkb->AddUsageStmtVar("10", "o");
+  pkb->AddUsageStmtVar("11", "c");
+  pkb->AddUsageStmtVar("11", "p");
+  pkb->AddUsageStmtVar("11", "q");
+  pkb->AddUsageStmtVar("12", "r");
+  pkb->AddUsageStmtVar("14", "s");
+  pkb->AddUsageStmtVar("14", "t");
+  pkb->AddUsageStmtVar("14", "u");
+  pkb->AddUsageStmtVar("15", "v");
+
 }
 
 TEST_CASE("PKB instance") {
@@ -45,7 +135,16 @@ TEST_CASE("PKB instance") {
   REQUIRE(pkb1 == pkb2);
 }
 
-TEST_CASE("stmt s; select s;") {
+TEST_CASE("proc p; select p") {
+  set_up_pkb();
+  PKB *pkb = PKB::GetInstance();
+  auto actual = pkb->GetStmt(PROC);
+  auto expected = proc_list;
+
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("stmt s; select s") {
   set_up_pkb();
   PKB *pkb = PKB::GetInstance();
   auto actual = pkb->GetStmt(STMT);
@@ -54,7 +153,7 @@ TEST_CASE("stmt s; select s;") {
   REQUIRE(actual == expected);
 }
 
-TEST_CASE("variable v; select v;") {
+TEST_CASE("variable v; select v") {
   set_up_pkb();
   PKB *pkb = PKB::GetInstance();
   auto actual = pkb->GetStmt(VARS);
@@ -63,11 +162,65 @@ TEST_CASE("variable v; select v;") {
   REQUIRE(actual == expected);
 }
 
-TEST_CASE("assign a; select a;") {
+TEST_CASE("assign a; select a") {
   set_up_pkb();
   PKB *pkb = PKB::GetInstance();
   auto actual = pkb->GetStmt(ASSIGN);
-  auto expected = assign_stmt_list;
+  auto expected = assign_list;
+
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("if ifs; select ifs") {
+  set_up_pkb();
+  PKB *pkb = PKB::GetInstance();
+  auto actual = pkb->GetStmt(IF);
+  auto expected = if_list;
+
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("while w; select w") {
+  set_up_pkb();
+  PKB *pkb = PKB::GetInstance();
+  auto actual = pkb->GetStmt(ASSIGN);
+  auto expected = while_list;
+
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("const c; select c") {
+  set_up_pkb();
+  PKB *pkb = PKB::GetInstance();
+  auto actual = pkb->GetStmt(CONSTS);
+  auto expected = const_list;
+
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("read r; select r") {
+  set_up_pkb();
+  PKB *pkb = PKB::GetInstance();
+  auto actual = pkb->GetStmt(READ);
+  auto expected = read_list;
+
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("print p; select p") {
+  set_up_pkb();
+  PKB *pkb = PKB::GetInstance();
+  auto actual = pkb->GetStmt(PRINT);
+  auto expected = print_list;
+
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("Get var used by stmt") {
+  set_up_pkb();
+  PKB *pkb = PKB::GetInstance();
+  auto actual = pkb->GetVarUsedByStmt("7");
+  auto expected = stmt_to_var.at("7");
 
   REQUIRE(actual == expected);
 }
