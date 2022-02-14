055bc5b59d7755b0b08257065fe979da2fdfdf62
YuHeem <54734556+yuheem@users.noreply.github.com>
2022-02-15 03:23:29 +0800

    Update skeleton for query evaluator

--- Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/query_evaluator.cpp
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/query_evaluator.cpp
@@ -2,15 +2,15 @@
 
 namespace pql_evaluator {
 
-void QueryEvaluator::Evaluate(ParsedQuery& query, std::list<std::string>& results) {
+void QueryEvaluator::Evaluate(ParsedQuery &query, std::list<std::string> &results) {
   result.clear();
   EvaluateSelect(query);
-  for (auto i = result.begin(); i != result.end(); ++i) {
+  for (auto i = result.begin(); i!=result.end(); ++i) {
     results.emplace_back(*i);
   }
 }
 
-void QueryEvaluator::EvaluateSelect(ParsedQuery& query) {
+void QueryEvaluator::EvaluateSelect(ParsedQuery &query) {
   const PqlToken select_synonym = query.GetSynonym();
   const auto declarations = query.GetDeclaration();
   PqlToken token; // uninit
@@ -18,7 +18,7 @@ void QueryEvaluator::EvaluateSelect(ParsedQuery& query) {
   // pass thru validation
   // TODO: refactor ParsedQuery to ValidQuery object
   for (auto declaration : declarations) {
-    if (declaration.GetSynonym().value == select_synonym.value) {
+    if (declaration.GetSynonym().value==select_synonym.value) {
       token = declaration.GetDesignEntity();
     }
   }
@@ -69,18 +69,34 @@ void QueryEvaluator::EvaluateSelect(ParsedQuery& query) {
     }
   }
 
-  // if such that clause but no pattern
-  if (!query.GetRelationships().empty() && query.GetPatterns().empty()) {
+    // if such that clause but no pattern
+  else if (!query.GetRelationships().empty() && query.GetPatterns().empty()) {
+    Relationship relationship = query.GetRelationships().front();
+    PqlTokenType rel_ref = relationship.GetRelRef().type;
+    PqlToken first_arg = relationship.GetFirst();
+    PqlToken second_arg = relationship.GetSecond();
 
+    if (rel_ref == PqlTokenType::USES) {
+      // Uses(s, v)
+      // Uses(s, _)
+      // Uses(s, "x")
+      // Uses(1, v)
+      // Uses(1, _)
+      // Uses(1, "x")
+
+
+
+    } else if (rel_ref == PqlTokenType::MODIFIES) {
+
+    }
   }
 
   // if no such that clause but has pattern
-  if (query.GetRelationships().empty() && !query.GetPatterns().empty()) {
+  else if (query.GetRelationships().empty() && !query.GetPatterns().empty()) {
 
   }
 
-  // if both such that clause and pattern
-  if (!query.GetRelationships().empty() && !query.GetPatterns().empty()) {
+  else {  // both clauses present
 
   }
 }
