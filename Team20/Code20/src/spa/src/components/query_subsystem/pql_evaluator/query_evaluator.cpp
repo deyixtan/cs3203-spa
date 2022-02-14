3b7fccffe57342d5ac2ead82df46b2d82e94f58a
YuHeem <54734556+yuheem@users.noreply.github.com>
2022-02-15 02:59:16 +0800

    Add evaluation of select only

--- Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/query_evaluator.cpp
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/query_evaluator.cpp
@@ -16,57 +16,59 @@ void QueryEvaluator::EvaluateSelect(ParsedQuery& query) {
   PqlToken token; // uninit
 
   // pass thru validation
+  // TODO: refactor ParsedQuery to ValidQuery object
   for (auto declaration : declarations) {
     if (declaration.GetSynonym().value == select_synonym.value) {
       token = declaration.GetDesignEntity();
     }
   }
 
-  std::unordered_set<std::string> add_result;
-  switch (token.type) {
-    case PqlTokenType::STMT: {
-      add_result = pkb->GetStmt(StmtType::STMT);
-      result.insert(add_result.begin(), add_result.end());
-      break;
-    }
-    case PqlTokenType::READ: {
-      add_result = pkb->GetStmt(StmtType::READ);
-      result.insert(add_result.begin(), add_result.end());
-    }
-    case PqlTokenType::PRINT: {
-      add_result = pkb->GetStmt(StmtType::PRINT);
-      result.insert(add_result.begin(), add_result.end());
-    }
-    case PqlTokenType::WHILE: {
-      add_result = pkb->GetStmt(StmtType::WHILE);
-      result.insert(add_result.begin(), add_result.end());
-    }
-    case PqlTokenType::IF: {
-      add_result = pkb->GetStmt(StmtType::IF);
-      result.insert(add_result.begin(), add_result.end());
-    }
-    case PqlTokenType::VARIABLE: {
-      add_result = pkb->GetStmt(StmtType::VARS);
-      result.insert(add_result.begin(), add_result.end());
-      break;
-    }
-    case PqlTokenType::CONSTANT: {
-      add_result = pkb->GetStmt(StmtType::CONSTS);
-      result.insert(add_result.begin(), add_result.end());
-      break;
-    }
-    case PqlTokenType::PROCEDURE: {
-      add_result = pkb->GetStmt(StmtType::PROC);
-      result.insert(add_result.begin(), add_result.end());
-      break;
-    }
-    default: {
-      // TODO:throw exception
+  // if no such that and no pattern
+  if (query.GetRelationships().empty() && query.GetPatterns().empty()) {
+    std::unordered_set<std::string> add_result;
+    switch (token.type) {
+      case PqlTokenType::STMT: {
+        add_result = pkb->GetStmt(StmtType::STMT);
+        result.insert(add_result.begin(), add_result.end());
+        break;
+      }
+      case PqlTokenType::READ: {
+        add_result = pkb->GetStmt(StmtType::READ);
+        result.insert(add_result.begin(), add_result.end());
+      }
+      case PqlTokenType::PRINT: {
+        add_result = pkb->GetStmt(StmtType::PRINT);
+        result.insert(add_result.begin(), add_result.end());
+      }
+      case PqlTokenType::WHILE: {
+        add_result = pkb->GetStmt(StmtType::WHILE);
+        result.insert(add_result.begin(), add_result.end());
+      }
+      case PqlTokenType::IF: {
+        add_result = pkb->GetStmt(StmtType::IF);
+        result.insert(add_result.begin(), add_result.end());
+      }
+      case PqlTokenType::VARIABLE: {
+        add_result = pkb->GetStmt(StmtType::VARS);
+        result.insert(add_result.begin(), add_result.end());
+        break;
+      }
+      case PqlTokenType::CONSTANT: {
+        add_result = pkb->GetStmt(StmtType::CONSTS);
+        result.insert(add_result.begin(), add_result.end());
+        break;
+      }
+      case PqlTokenType::PROCEDURE: {
+        add_result = pkb->GetStmt(StmtType::PROC);
+        result.insert(add_result.begin(), add_result.end());
+        break;
+      }
+      default: {
+        // TODO:throw exception
+      }
     }
   }
-  // TODO:
-  //  1. call pkb to get based on type of select synonym
-  //  2. populate the result set
+
 }
 
 }
