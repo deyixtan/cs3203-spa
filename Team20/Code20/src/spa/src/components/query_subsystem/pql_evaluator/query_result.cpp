46d19dd5e5fb2e899b3df0838fb02aced71b1ce2
ddanelynn <dingyoujia0823@gmail.com>
2022-02-14 16:16:11 +0800

    add more test cases

--- Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/query_result.cpp
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/query_result.cpp
@@ -26,40 +26,51 @@ std::unordered_set<std::string> QueryResult::GetResult(PqlToken selected_synonym
       }
     }
   } else {
+    int synonym_count = 0;
     if (first_condition_pair.first.first_arg == selected_synonym) {
       for (auto result_pair : first_condition_pair.second) {
         result_list.push_back(result_pair.first);
       }
+      synonym_count++;
     }
     if (first_condition_pair.first.second_arg == selected_synonym) {
       for (auto result_pair : first_condition_pair.second) {
         result_list.push_back(result_pair.second);
       }
+      synonym_count++;
     }
     if (second_condition_pair.first.first_arg == selected_synonym) {
       for (auto result_pair : second_condition_pair.second) {
         result_list.push_back(result_pair.first);
       }
+      synonym_count++;
     }
     if (second_condition_pair.first.second_arg == selected_synonym) {
       for (auto result_pair : second_condition_pair.second) {
         result_list.push_back(result_pair.second);
       }
+      synonym_count++;
     }
 
-    std::unordered_map<std::string, int> result_map;
+    if (synonym_count > 1) {
+      std::unordered_map<std::string, int> result_map;
 
-    for (auto result_string : result_list) {
-      if (result_map.find(result_string) == result_map.end()) {
-        result_map.insert(make_pair(result_string, 1));
-      } else {
-        result_map.insert(std::make_pair(result_string, result_map[result_string] + 1));
+      for (auto result_string : result_list) {
+        if (result_map.find(result_string) == result_map.end()) {
+          result_map.insert(make_pair(result_string, 1));
+        } else {
+          result_map.find(result_string)->second = result_map.at(result_string) + 1;
+        }
       }
-    }
 
-    for (auto key_value_pair : result_map) {
-      if (key_value_pair.second > 1) {
-        result.insert(key_value_pair.first);
+      for (auto key_value_pair : result_map) {
+        if (key_value_pair.second > 1) {
+          result.insert(key_value_pair.first);
+        }
+      }
+    } else {
+      for (auto key_value_pair : result_list) {
+        result.insert(key_value_pair);
       }
     }
   }
