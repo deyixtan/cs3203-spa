052bb035a6f682c7793a23ebb86a10b4094a032b
YuHeem <54734556+yuheem@users.noreply.github.com>
2022-02-15 14:24:31 +0800

    Update table header and implement constructors

--- Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.cpp
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.cpp
@@ -1,1 +1,21 @@
 #include "table.h"
+
+namespace pql {
+
+Table::Table(std::string synonym, std::unordered_set<std::string> possible_single_values) {
+  synonym_to_col_idx_map_[synonym] = next_col_idx_++;
+  for (auto possible_single_value : possible_single_values) {
+    table_.push_back(std::vector{possible_single_value});
+  }
+}
+
+Table::Table(std::string first_synonym, std::string second_synonym,
+             std::unordered_set<std::pair<std::string, std::string>> possible_pair_values){
+  synonym_to_col_idx_map_[first_synonym] = next_col_idx_++;
+  synonym_to_col_idx_map_[second_synonym] = next_col_idx_++;
+  for (auto possible_pair_value : possible_pair_values) {
+    table_.push_back(std::vector{possible_pair_value.first, possible_pair_value.second});
+  }
+}
+
+}
--- Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.h
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.h
@@ -12,11 +12,15 @@ namespace pql {
 /**
  * Table is an abstraction of the possible results that a relation of synonyms can take.
  * A synonym maps to a certain column in the table.
+ *
+ * In general we only construct tables with a max of two synonyms since we have
+ * a maximum of two synonyms for clause arguments.
+ *
  */
 class Table {
  public:
-  void AddOneColumn(std::string synonym, std::unordered_set<std::string> possible_single_values);
-  void AddTwoColumns(std::string first_synonym, std::string second_synonym,
+  Table(std::string synonym, std::unordered_set<std::string> possible_single_values);
+  Table(std::string first_synonym, std::string second_synonym,
                      std::unordered_set<std::pair<std::string, std::string>> possible_pair_values);
   void NaturalJoin(Table& other_table);
   void CrossJoin(Table& other_table);
