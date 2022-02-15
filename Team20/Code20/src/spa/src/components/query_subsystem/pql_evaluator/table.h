52dc1dad6fa8fb8aa9b295514eb7db6f84a72ce8
YuHeem <54734556+yuheem@users.noreply.github.com>
2022-02-15 14:00:58 +0800

    Update table header

---
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.h
@@ -0,0 +1,31 @@
+#ifndef SPA_SRC_SPA_SRC_COMPONENTS_QUERY_SUBSYSTEM_PQL_EVALUATOR_TABLE_H_
+#define SPA_SRC_SPA_SRC_COMPONENTS_QUERY_SUBSYSTEM_PQL_EVALUATOR_TABLE_H_
+
+#include <pair>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace pql {
+
+/**
+ * Table is an abstraction of the possible results that a relation of synonyms can take.
+ * A synonym maps to a certain column in the table.
+ */
+class Table {
+ public:
+  void AddOneColumn(std::string synonym, std::unordered_set<std::string> possible_single_values);
+  void AddTwoColumns(std::string first_synonym, std::string second_synonym,
+                     std::unordered_set<std::pair<std::string, std::string>> possible_pair_values);
+  void NaturalJoin(Table& other_table);
+  void CrossJoin(Table& other_table);
+ private:
+  std::vector<std::vector<std::string>> table_;
+  std::unordered_map<std::string, size_t> synonym_to_col_idx_map_;
+  size_t next_col_idx_ = 0;
+};
+
+}
+
+#endif //SPA_SRC_SPA_SRC_COMPONENTS_QUERY_SUBSYSTEM_PQL_EVALUATOR_TABLE_H_
