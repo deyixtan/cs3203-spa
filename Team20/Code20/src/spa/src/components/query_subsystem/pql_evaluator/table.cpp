dc4c3750930432d06adb9181a2127362be5520d4
YuHeem <54734556+yuheem@users.noreply.github.com>
2022-02-15 15:40:26 +0800

    Update table implementation

--- Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.cpp
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.cpp
@@ -18,4 +18,53 @@ Table::Table(std::string first_synonym, std::string second_synonym,
   }
 }
 
+std::unordered_set<std::string> Table::GetSharedSynonyms(Table& other_table) {
+  std::unordered_set<std::string> synonyms;
+  std::unordered_set<std::string> other_synonyms;
+  for (auto kv : synonym_to_col_idx_map_) {
+    synonyms.insert(kv.first);
+  }
+  for (auto kv : other_table.synonym_to_col_idx_map_) {
+    other_synonyms.insert(kv.first);
+  }
+
+  std::unordered_set<std::string> common_synonyms;
+  if (synonyms.size() < other_synonyms.size()) {
+    for (auto synonym : synonyms) {
+      if (other_synonyms.find(synonym) != other_synonyms.end()) {
+        common_synonyms.insert(synonym);
+      }
+    }
+  } else {
+    for (auto other_synonym : other_synonyms) {
+      if (synonyms.find(other_synonym) != synonyms.end()) {
+        common_synonyms.insert(other_synonym);
+
+      }
+    }
+  }
+
+  return common_synonyms;
+}
+
+bool Table::HasSharedSynonym(Table &other_table) {
+  return !GetSharedSynonyms(other_table).empty();
+}
+
+void Table::Join(Table &other_table) {
+  if (HasSharedSynonym(other_table)) {
+    NaturalJoin(other_table);
+  } else {
+    CrossJoin(other_table);
+  }
+}
+
+void NaturalJoin(Table &other_table) {
+
+}
+
+void CrossJoin(Table& other_table) {
+
+}
+
 }
--- Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.h
+++ Team20/Code20/src/spa/src/components/query_subsystem/pql_evaluator/table.h
@@ -19,15 +19,21 @@ namespace pql {
  */
 class Table {
  public:
+  std::unordered_map<std::string, size_t> synonym_to_col_idx_map_;
+
   Table(std::string synonym, std::unordered_set<std::string> possible_single_values);
   Table(std::string first_synonym, std::string second_synonym,
                      std::unordered_set<std::pair<std::string, std::string>> possible_pair_values);
+  void Join(Table& other_table);
   void NaturalJoin(Table& other_table);
   void CrossJoin(Table& other_table);
+
  private:
   std::vector<std::vector<std::string>> table_;
-  std::unordered_map<std::string, size_t> synonym_to_col_idx_map_;
   size_t next_col_idx_ = 0;
+
+  std::unordered_set<std::string> GetSharedSynonyms(Table& other_table);
+  bool HasSharedSynonym(Table& other_table);
 };
 
 }
