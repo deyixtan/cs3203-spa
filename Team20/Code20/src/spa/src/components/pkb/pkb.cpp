623b85b455e7bec3bdb153dd01e62d3efb9f1f5c
Jaryl Loh <jarylloh1997@gmail.com>
2022-02-19 02:12:43 +0800

    Fix errors in double synonym follows

--- Team20/Code20/src/spa/src/components/pkb/pkb.cpp
+++ Team20/Code20/src/spa/src/components/pkb/pkb.cpp
@@ -365,7 +365,7 @@ std::unordered_set<std::pair<std::string, std::string>, pair_hash> PKB::GetAllFo
     case STMT:
       for (auto i : follow_stmt_list) {
         for (auto j : stmt_list) {
-          if (i.first == j) {
+          if (i.second == j) {
             result.insert(i);
           }
         }
@@ -374,7 +374,7 @@ std::unordered_set<std::pair<std::string, std::string>, pair_hash> PKB::GetAllFo
     case READ:
       for (auto i : follow_stmt_list) {
         for (auto j : read_stmt_list) {
-          if (i.first == j) {
+          if (i.second == j) {
             result.insert(i);
           }
         }
@@ -383,7 +383,7 @@ std::unordered_set<std::pair<std::string, std::string>, pair_hash> PKB::GetAllFo
     case ASSIGN:
       for (auto i : follow_stmt_list) {
         for (auto j : assign_stmt_list) {
-          if (i.first == j) {
+          if (i.second == j) {
             result.insert(i);
           }
         }
@@ -392,7 +392,7 @@ std::unordered_set<std::pair<std::string, std::string>, pair_hash> PKB::GetAllFo
     case WHILE:
       for (auto i : follow_stmt_list) {
         for (auto j : while_stmt_list) {
-          if (i.first == j) {
+          if (i.second == j) {
             result.insert(i);
           }
         }
@@ -410,7 +410,7 @@ std::unordered_set<std::pair<std::string, std::string>, pair_hash> PKB::GetAllFo
     case IF:
       for (auto i : follow_stmt_list) {
         for (auto j : if_stmt_list) {
-          if (i.first == j) {
+          if (i.second == j) {
             result.insert(i);
           }
         }
