b5ea25c0d49660eef657e4da04b2a7012a43f625
Deyi Tan <deyixtan@gmail.com>
2022-02-20 05:20:01 +0800

    Add integration tests for sample source 2

--- Team20/Code20/src/integration_testing/src/data/sample_source.cpp
+++ Team20/Code20/src/integration_testing/src/data/sample_source.cpp
@@ -2,43 +2,43 @@
 
 std::string sample_source1 =
     "procedure computeCentroid {"
-    "    count = 0;"
-    "    cenX = 0;"
-    "    cenY = 0;"
-    "    while ((x != 0) && (y != 0)) {"
-    "        count = count + 1;"
-    "        cenX = cenX + x;"
-    "        cenY = cenY + y;"
+    "    count = 0;" // 1
+    "    cenX = 0;" // 2
+    "    cenY = 0;" // 3
+    "    while ((x != 0) && (y != 0)) {" // 4
+    "        count = count + 1;" // 5
+    "        cenX = cenX + x;" // 6
+    "        cenY = cenY + y;" // 7
     "    }"
-    "    if (count == 0) then {"
-    "        flag = 1;"
+    "    if (count == 0) then {" // 8
+    "        flag = 1;" // 9
     "    } else {"
-    "        cenX = cenX / count;"
-    "        cenY = cenY / count;"
+    "        cenX = cenX / count;" // 10
+    "        cenY = cenY / count;" // 11
     "    }"
-    "    normSq = cenX * cenX + cenY * cenY;"
+    "    normSq = cenX * cenX + cenY * cenY;" // 12
     "}";
 
 std::string sample_source2 =
     "procedure bar {"
-    "    read x;"
-    "    read y;"
-    "    if ((x >= 0) && (y >= 0)) then {"
-    "        while (x != 0) {"
-    "            tan = y / x;"
-    "            sin = y / length;"
-    "            cos = x / length;"
-    "            if (((sin * sin + cos * cos) == 1) || (!(tan != (sin / cos)))) then {"
-    "                x = y / 2;"
-    "                y = x / 2;"
+    "    read x;" // 1
+    "    read y;" // 2
+    "    if ((x >= 0) && (y >= 0)) then {" // 3
+    "        while (x != 0) {" // 4
+    "            tan = y / x;" // 5
+    "            sin = y / length;" // 6
+    "            cos = x / length;" // 7
+    "            if (((sin * sin + cos * cos) == 1) || (!(tan != (sin / cos)))) then {" // 8
+    "                x = y / 2;" // 9
+    "                y = x / 2;" // 10
     "            } else {"
-    "                x = 0;"
+    "                x = 0;" // 11
     "            }"
-    "            length = x * x + y * y;"
+    "            length = x * x + y * y;" // 12
     "        }"
     "    } else {"
-    "        x = 10;"
-    "        y = 10;"
-    "        length = x * x + y * y;"
+    "        x = 10;" // 13
+    "        y = 10;" // 14
+    "        length = x * x + y * y;" // 15
     "    }"
     "}";
