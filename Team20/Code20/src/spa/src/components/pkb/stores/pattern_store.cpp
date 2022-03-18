84aecd66d51bbfd098f80f5b3de9b6b8c6ed1a57
Jaryl Loh <jarylloh1997@gmail.com>
2022-03-18 12:48:24 +0800

    Change if-while pattern implementation

--- Team20/Code20/src/spa/src/components/pkb/stores/pattern_store.cpp
+++ Team20/Code20/src/spa/src/components/pkb/stores/pattern_store.cpp
@@ -34,68 +34,110 @@ void PatternStore::AddIfWithPattern(std::string const &stmt, std::string const &
   }
 }
 
-std::unordered_set<std::string> PatternStore::GetStmtWithPattern(std::string lhs, std::string rhs) {
-  ExpressionTree expr_tree = ExpressionTree();
+std::unordered_set<std::string> PatternStore::GetStmtWithPatternExact(std::string lhs, std::string rhs) {
   std::unordered_set<std::string> result = {};
   lhs.erase(remove(lhs.begin(), lhs.end(), ' '), lhs.end());
   rhs.erase(remove(rhs.begin(), rhs.end(), ' '), rhs.end());
-  std::string temp, sub_pattern;
 
-  if (rhs.find('_') != std::string::npos) {
-    auto first = rhs.find("_\"");
-    auto last = rhs.find("\"_");
-    temp = "(" + rhs.substr(first + 2, last - 2) + ")";
-  } else {
-    temp = "(" + rhs + ")";
-  }
-
-  if (temp != "()") {
-    expr_tree.build(temp);
-    sub_pattern = expr_tree.GetPattern(expr_tree.GetRoot());
-  }
+  ExpressionTree expr_tree = ExpressionTree();
+  expr_tree.build(rhs);
+  std::string sub_pattern = expr_tree.GetPattern(expr_tree.GetRoot());
 
   for (auto const&[key, val] : m_stmt_pattern_map) {
-    //pattern a(_, _)
-    if (lhs == "_" && rhs == "_") {
-      result.insert(key);
-    }
-
-    //PARTIAL MATCH RHS: pattern a(_, _"x+1"_)
-    if (lhs == "_" && rhs != "_" && rhs.find('_') != std::string::npos) {
-      if (val.second.find(sub_pattern) != std::string::npos) {
-        result.insert(key);
-      }
-    }
-
     //EXACT MATCH RHS: pattern a(_, "x+1")
-    if (lhs == "_" && rhs != "_" && rhs.find('_') == std::string::npos) {
-      if (val.second == rhs) {
-        result.insert(key);
-      }
-    }
-
-    //EXACT MATCH LHS: pattern a("x", _)
-    if (lhs != "_" && rhs == "_") {
-      if (lhs == val.first) {
-        result.insert(key);
-      }
-    }
-
-    //EXACT MATCH LHS + PARTIAL MATCH RHS: pattern a("x", _"x+1"_)
-    if (lhs != "_" && rhs != "_" && rhs.find('_') != std::string::npos) {
-      if (lhs == val.first && val.second.find(sub_pattern) != std::string::npos) {
+    if (lhs == "_" && rhs != "_") {
+      if (val.second == sub_pattern) {
         result.insert(key);
       }
     }
 
     //EXACT MATCH LHS + EXACT MATCH RHS: pattern a("x", "x+1")
-    if (lhs != "_" && rhs != "_" && rhs.find('_') == std::string::npos) {
-      if (lhs == val.first && val.second == rhs) {
+    if (lhs != "_" && rhs != "_") {
+      if (lhs == val.first && val.second == sub_pattern) {
         result.insert(key);
       }
     }
   }
   return result;
+}
+
+std::unordered_set<std::string> PatternStore::GetStmtWithPatternPartial(std::string lhs, std::string rhs) {
+  std::unordered_set<std::string> result = {};
+  lhs.erase(remove(lhs.begin(), lhs.end(), ' '), lhs.end());
+  rhs.erase(remove(rhs.begin(), rhs.end(), ' '), rhs.end());
+
+  ExpressionTree expr_tree = ExpressionTree();
+  expr_tree.build(rhs);
+  std::string sub_pattern = expr_tree.GetPattern(expr_tree.GetRoot());
+
+  for (auto const&[key, val] : m_stmt_pattern_map) {
+    //PARTIAL MATCH RHS: pattern a(_, _"x+1"_)
+    if (lhs == "_") {
+      if (val.second.find(sub_pattern) != std::string::npos) {
+        result.insert(key);
+      }
+    } else { //EXACT MATCH LHS + PARTIAL MATCH RHS: pattern a("x", _"x+1"_)
+      if (lhs == val.first && val.second.find(sub_pattern) != std::string::npos) {
+        result.insert(key);
+      }
+    }
+  }
+  return result;
+}
+
+std::unordered_set<std::string> PatternStore::GetStmtWithPatternWildcard(std::string lhs) {
+  std::unordered_set<std::string> result = {};
+  lhs.erase(remove(lhs.begin(), lhs.end(), ' '), lhs.end());
+
+  for (auto const&[key, val] : m_stmt_pattern_map) {
+    //pattern a(_, _)
+    if (lhs == "_") {
+      result.insert(key);
+    } else { //EXACT MATCH LHS: pattern a("x", _)
+      if (lhs == val.first) {
+        result.insert(key);
+      }
+    }
+  }
+  return result;
+}
+
+std::unordered_set<std::pair<std::string, std::string>, pair_hash> PatternStore::GetStmtWithPatternSynonymExact(std::string expr) {
+  std::unordered_set<std::pair<std::string, std::string>, pair_hash> result = {};
+  expr.erase(remove(expr.begin(), expr.end(), ' '), expr.end());
+
+  ExpressionTree expr_tree = ExpressionTree();
+  expr_tree.build(expr);
+  std::string sub_pattern = expr_tree.GetPattern(expr_tree.GetRoot());
+
+  for (auto const&[key, val] : m_stmt_pattern_map) {
+    //EXACT MATCH RHS: pattern a(v, "x+1")
+    if (val.second == sub_pattern) {
+      result.insert({key, val.first});
+    }
+  }
+  return result;
+}
+
+std::unordered_set<std::pair<std::string, std::string>, pair_hash> PatternStore::GetStmtWithPatternSynonymPartial(std::string expr) {
+  std::unordered_set<std::pair<std::string, std::string>, pair_hash> result = {};
+  expr.erase(remove(expr.begin(), expr.end(), ' '), expr.end());
+
+  ExpressionTree expr_tree = ExpressionTree();
+  expr_tree.build(expr);
+  std::string sub_pattern = expr_tree.GetPattern(expr_tree.GetRoot());
+
+  for (auto const&[key, val] : m_stmt_pattern_map) {
+    //PARTIAL MATCH RHS: pattern a(v, _"x+1"_)
+    if (val.second.find(sub_pattern) != std::string::npos) {
+      result.insert({key, val.first});
+    }
+  }
+  return result;
+}
+
+std::unordered_set<std::pair<std::string, std::string>, pair_hash> PatternStore::GetStmtWithPatternSynonymWildcard() {
+  return m_stmt_pattern_pairs;
 }
 
 std::unordered_set<std::string> PatternStore::GetIfWithPattern(std::string var) {
@@ -129,49 +171,6 @@ std::unordered_set<std::string> PatternStore::GetWhileWithPattern(std::string va
     //pattern w("x", _)
     if (var != "_" && pair.second.find(sub_pattern) != std::string::npos) {
       result.insert(pair.first);
-    }
-  }
-  return result;
-}
-
-std::unordered_set<std::pair<std::string, std::string>, pair_hash> PatternStore::GetStmtWithPatternSynonym(std::string expr) {
-  std::unordered_set<std::pair<std::string, std::string>, pair_hash> result = {};
-
-  //pattern a(v, _)
-  if (expr == "_") {
-    result = m_stmt_pattern_pairs;
-  } else {
-    for (auto const&[key, val] : m_stmt_pattern_map) {
-      ExpressionTree expr_tree = ExpressionTree();
-      expr.erase(remove(expr.begin(), expr.end(), ' '), expr.end());
-      std::string temp, sub_pattern;
-
-      //pattern a(v, "x")
-      if (expr.find('_') == std::string::npos) {
-        expr_tree.build(expr);
-        sub_pattern = expr_tree.GetPattern(expr_tree.GetRoot());
-        if (val.second == sub_pattern) {
-          result.insert({key, val.first});
-        }
-      }
-
-      //pattern a(v, _"x"_)
-      if (expr.find('_') != std::string::npos) {
-        if (expr.find('_') != std::string::npos) {
-          auto first = expr.find("_\"");
-          auto last = expr.find("\"_");
-          temp = "(" + expr.substr(first + 2, last - 2) + ")";
-        } else {
-          temp = "(" + expr + ")";
-        }
-
-        expr_tree.build(temp);
-        sub_pattern = expr_tree.GetPattern(expr_tree.GetRoot());
-
-        if (val.second.find(sub_pattern) != std::string::npos) {
-          result.insert({key, val.first});
-        }
-      }
     }
   }
   return result;
