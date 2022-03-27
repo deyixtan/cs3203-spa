e89e38ec60274d9db3b2507873566a725377d1ad
Deyi Tan <deyixtan@gmail.com>
2022-03-27 19:45:13 +0800

    Update AffectStore with ModifyStore and UsageStore references

--- Team20/Code20/src/spa/src/components/pkb/stores/affect_store.cpp
+++ Team20/Code20/src/spa/src/components/pkb/stores/affect_store.cpp
@@ -1,15 +1,20 @@
 #include "affect_store.h"
 #include "affect_session.h"
+#include "modify_store.h"
+#include "usage_store.h"
 #include "../../source_subsystem/types/cfg/cfg.h"
 
-AffectStore::AffectStore(std::shared_ptr<std::vector<std::unordered_set<std::string>>> stmt_vector) :
-    Store(move(stmt_vector)) {}
+AffectStore::AffectStore(std::shared_ptr<std::vector<std::unordered_set<std::string>>> stmt_vector,
+                         std::shared_ptr<ModifyStore> modify_store,
+                         std::shared_ptr<UsageStore> usage_store) :
+    Store(move(stmt_vector)), m_modify_store(modify_store), m_usage_store(usage_store) {}
 
 void AffectStore::AddProgramCfg(std::shared_ptr<Cfg> program_cfg) {
   m_program_cfg = program_cfg;
 }
 
 std::shared_ptr<AffectSession> AffectStore::ComputeAffect() {
-  std::shared_ptr<AffectSession> affect_session = std::make_shared<AffectSession>(m_program_cfg);
+  std::shared_ptr<AffectSession>
+      affect_session = std::make_shared<AffectSession>(m_program_cfg, m_modify_store, m_usage_store);
   return affect_session;
 }
--- Team20/Code20/src/spa/src/components/pkb/stores/affect_store.h
+++ Team20/Code20/src/spa/src/components/pkb/stores/affect_store.h
@@ -8,15 +8,21 @@
 
 #include "store.h"
 
-class Cfg;
 class AffectSession;
+class ModifyStore;
+class UsageStore;
+class Cfg;
 
 class AffectStore : public Store {
  private:
   std::shared_ptr<Cfg> m_program_cfg;
+  std::shared_ptr<ModifyStore> m_modify_store;
+  std::shared_ptr<UsageStore> m_usage_store;
 
  public:
-  explicit AffectStore(std::shared_ptr<std::vector<std::unordered_set<std::string>>> stmt_vector);
+  explicit AffectStore(std::shared_ptr<std::vector<std::unordered_set<std::string>>> stmt_vector,
+                       std::shared_ptr<ModifyStore> modify_store,
+                       std::shared_ptr<UsageStore> usage_store);
   void AddProgramCfg(std::shared_ptr<Cfg> program_cfg);
   std::shared_ptr<AffectSession> ComputeAffect();
 };
