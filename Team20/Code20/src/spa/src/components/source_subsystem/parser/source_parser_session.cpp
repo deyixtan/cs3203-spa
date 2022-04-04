5affed49d5674f7b15fa62051af995fb308a0450
Deyi Tan <deyixtan@gmail.com>
2022-04-05 04:32:18 +0800

    Abstract cyclic call detection algorithm into multiple methods

--- Team20/Code20/src/spa/src/components/source_subsystem/parser/source_parser_session.cpp
+++ Team20/Code20/src/spa/src/components/source_subsystem/parser/source_parser_session.cpp
@@ -7,20 +7,7 @@ std::string SourceParserSession::GetCurrProcedure() {
   return m_curr_parsed_procedure;
 }
 
-bool SourceParserSession::DoesProcedureExist(std::string const &procedure_name) {
-  return m_procedures_parsed_set.find(procedure_name) != m_procedures_parsed_set.end();
-}
-
-bool SourceParserSession::DoesInvalidCallExist() {
-  auto predicate = [this](const std::string &procedure_called) {
-    return m_procedures_parsed_set.find(procedure_called) == m_procedures_parsed_set.end();
-  };
-  return std::any_of(m_procedures_called_set.begin(), m_procedures_called_set.end(), predicate);
-}
-
-bool SourceParserSession::DoesCyclicCallExist() {
-  // using Kahn algorithm to check for cycles
-
+std::unordered_map<std::string, int> SourceParserSession::GetInitKahnInDegreeMap() {
   // init in-degree map
   std::unordered_map<std::string, int> in_degree_map;
   for (auto const &procedure_parsed : m_procedures_parsed_set) {
@@ -35,6 +22,10 @@ bool SourceParserSession::DoesCyclicCallExist() {
     }
   }
 
+  return in_degree_map;
+}
+
+std::queue<std::string> SourceParserSession::GetInitKahnQueue(std::unordered_map<std::string, int> &in_degree_map) {
   // populate queue with procedure with 0 in-degree
   std::queue<std::string> queue;
   for (auto const &[caller_name, in_degree_count] : in_degree_map) {
@@ -42,10 +33,12 @@ bool SourceParserSession::DoesCyclicCallExist() {
       queue.push(caller_name);
     }
   }
+  return queue;
+}
 
+void SourceParserSession::ProcessKahnQueue(std::unordered_map<std::string, int> &in_degree_map, std::queue<std::string> &queue, int &processed) {
   // process each procedure with 0 in-degree
   // at the end, if managed to process the same number of procedures being parsed = no cycles
-  int processed = 0;
   while (!queue.empty()) {
     std::string curr = queue.front();
     queue.pop();
@@ -60,7 +53,26 @@ bool SourceParserSession::DoesCyclicCallExist() {
 
     processed += 1;
   }
+}
 
+bool SourceParserSession::DoesProcedureExist(std::string const &procedure_name) {
+  return m_procedures_parsed_set.find(procedure_name) != m_procedures_parsed_set.end();
+}
+
+bool SourceParserSession::DoesInvalidCallExist() {
+  auto predicate = [this](const std::string &procedure_called) {
+    return m_procedures_parsed_set.find(procedure_called) == m_procedures_parsed_set.end();
+  };
+  return std::any_of(m_procedures_called_set.begin(), m_procedures_called_set.end(), predicate);
+}
+
+bool SourceParserSession::DoesCyclicCallExist() {
+  // using modified Kahn algorithm to check for cycles
+  std::unordered_map<std::string, int> in_degree_map = GetInitKahnInDegreeMap();
+  std::queue<std::string> queue = GetInitKahnQueue(in_degree_map);
+
+  int processed = 0;
+  ProcessKahnQueue(in_degree_map, queue, processed);
   return processed != m_procedures_parsed_set.size();
 }
 
--- Team20/Code20/src/spa/src/components/source_subsystem/parser/source_parser_session.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/parser/source_parser_session.h
@@ -12,6 +12,11 @@ class SourceParserSession {
   std::unordered_set<std::string> m_procedures_called_set;
   std::unordered_map<std::string, std::unordered_set<std::string>> m_call_map; // <caller, callees>
 
+ private:
+  [[nodiscard]] std::unordered_map<std::string, int> GetInitKahnInDegreeMap();
+  [[nodisacrd]] std::queue<std::string> GetInitKahnQueue(std::unordered_map<std::string, int> &in_degree_map);
+  void ProcessKahnQueue(std::unordered_map<std::string, int> &in_degree_map, std::queue<std::string> &queue, int &processed);
+
  public:
   [[nodiscard]] std::string GetCurrProcedure();
   [[nodiscard]] bool DoesProcedureExist(std::string const &procedure_name);
