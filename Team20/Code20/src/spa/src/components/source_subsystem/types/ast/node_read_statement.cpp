cfd5de154f5cdd5cef1ec4cf94ffed19d796091e
Deyi Tan <deyixtan@gmail.com>
2022-04-05 23:07:42 +0800

    Update ReadStatementNode defs

--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_read_statement.cpp
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_read_statement.cpp
@@ -1,15 +1,13 @@
 #include "node_read_statement.h"
 #include "../../iterator/design_extractor.h"
 #include "../../iterator/cfg_builder.h"
-#include "../cfg/cfg_node.h"
 
 namespace source {
 
-ReadStatementNode::ReadStatementNode(int stmt_no, std::shared_ptr<VariableNode> identifier)
-    : StatementNode(stmt_no),
-      m_identifier(identifier) {}
+ReadStatementNode::ReadStatementNode(String stmt_no, VariableNodePtr identifier)
+    : StatementNode(stmt_no), m_identifier(identifier) {}
 
-std::shared_ptr<VariableNode> ReadStatementNode::GetIdentifier() {
+VariableNodePtr ReadStatementNode::GetIdentifier() {
   return m_identifier;
 }
 
@@ -17,11 +15,7 @@ StmtType ReadStatementNode::GetStatementType() {
   return StmtType::READ;
 }
 
-std::string ReadStatementNode::ToString() {
-  return StatementNode::ToString() + "read " + m_identifier->ToString() + ";\n";
-}
-
-std::string ReadStatementNode::GetPatternFormat() {
+String ReadStatementNode::GetPatternFormat() {
   return "";
 }
 
@@ -30,17 +24,17 @@ bool ReadStatementNode::operator==(const StatementNode &other) const {
   return m_stmt_no == casted_other->m_stmt_no && *m_identifier == *(casted_other->m_identifier);
 }
 
-void ReadStatementNode::Accept(DesignExtractor *de, std::string proc_name) {
-  std::string stmt_num = std::to_string(GetStatementNumber());
-  std::string var_name = m_identifier->GetIdentifier();
+void ReadStatementNode::Accept(DesignExtractor *de, String proc_name) {
+  String stmt_num = GetStatementNumber();
+  String var_name = m_identifier->GetIdentifier();
   de->GetPkbClient()->PopulateTypeOfStmt(stmt_num, READ);
 
   de->GetPkbClient()->PopulateRead(de->GetVisited(), stmt_num, var_name);
   de->Visit(m_identifier, proc_name, false);
 }
 
-std::shared_ptr<CfgNode> ReadStatementNode::Accept(CfgBuilder *cb, std::shared_ptr<CfgNode> cfg_node) {
-  cfg_node->AddStatement(StmtType::READ, std::to_string(GetStatementNumber()));
+CfgNodePtr ReadStatementNode::Accept(CfgBuilder *cb, CfgNodePtr cfg_node) {
+  cfg_node->AddStatement(StmtType::READ, GetStatementNumber());
   return cfg_node;
 }
 
--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_read_statement.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_read_statement.h
@@ -1,6 +1,7 @@
-#ifndef SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_READ_STATEMENT_H_
-#define SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_READ_STATEMENT_H_
+#ifndef READ_STATEMENT_H
+#define READ_STATEMENT_H
 
+#include "components/source_subsystem/source_declarations.h"
 #include "node_statement.h"
 #include "node_variable.h"
 
@@ -8,19 +9,18 @@ namespace source {
 
 class ReadStatementNode : public StatementNode {
  private:
-  std::shared_ptr<VariableNode> m_identifier;
+  VariableNodePtr m_identifier;
 
  public:
-  ReadStatementNode(int stmt_no, std::shared_ptr<VariableNode> identifier);
-  [[nodiscard]] std::shared_ptr<VariableNode> GetIdentifier();
+  ReadStatementNode(String stmt_no, VariableNodePtr identifier);
+  [[nodiscard]] VariableNodePtr GetIdentifier();
   [[nodiscard]] StmtType GetStatementType() override;
-  [[nodiscard]] std::string ToString() override;
-  [[nodiscard]] std::string GetPatternFormat() override;
+  [[nodiscard]] String GetPatternFormat() override;
   [[nodiscard]] bool operator==(const StatementNode &other) const override;
-  void Accept(DesignExtractor *de, std::string proc_name);
-  std::shared_ptr<CfgNode> Accept(CfgBuilder *cb, std::shared_ptr<CfgNode> cfg_node);
+  void Accept(DesignExtractor *de, String proc_name);
+  CfgNodePtr Accept(CfgBuilder *cb, CfgNodePtr cfg_node);
 };
 
 }
 
-#endif //SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_READ_STATEMENT_H_
+#endif //READ_STATEMENT_H
