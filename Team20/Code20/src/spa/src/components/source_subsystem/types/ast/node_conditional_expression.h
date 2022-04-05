408a477ac09c8885214356ea0830c64a940c6007
Deyi Tan <deyixtan@gmail.com>
2022-04-05 23:53:38 +0800

    Update ConditionalExpressionNode defs

--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_conditional_expression.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_conditional_expression.h
@@ -1,24 +1,23 @@
-#ifndef SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_CONDITIONAL_EXPRESSION_H_
-#define SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_CONDITIONAL_EXPRESSION_H_
+#ifndef NODE_CONDITIONAL_EXPRESSION_H
+#define NODE_CONDITIONAL_EXPRESSION_H
 
+#include "components/source_subsystem/source_declarations.h"
 #include "node.h"
 #include "components/source_subsystem/pkb_client.h"
 
 namespace source {
 
 enum class ConditionalType {
-  BOOLEAN,
-  NOT,
-  RELATIONAL
+  BOOLEAN, NOT, RELATIONAL
 };
 
 class ConditionalExpressionNode : public Node {
  public:
   [[nodiscard]] virtual ConditionalType GetConditionalType() = 0;
   [[nodiscard]] virtual bool operator==(const ConditionalExpressionNode &other) const = 0;
-  virtual std::string Accept(DesignExtractor *de, std::string proc_name, bool is_uses) = 0;
+  virtual String Accept(DesignExtractor *de, String proc_name, bool is_uses) = 0;
 };
 
 }
 
-#endif //SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_CONDITIONAL_EXPRESSION_H_
+#endif //NODE_CONDITIONAL_EXPRESSION_H
