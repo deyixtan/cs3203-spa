5d695d7037d564f5ced5c36f8b66372d8c5d8eb8
Deyi Tan <deyixtan@gmail.com>
2022-04-05 23:56:50 +0800

    Update ExpressionNode defs

--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_expression.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_expression.h
@@ -1,24 +1,23 @@
-#ifndef SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_EXPRESSION_H_
-#define SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_EXPRESSION_H_
+#ifndef NODE_EXPRESSION_H
+#define NODE_EXPRESSION_H
 
+#include "components/source_subsystem/source_declarations.h"
 #include "node.h"
 #include "components/source_subsystem/pkb_client.h"
 
 namespace source {
 
 enum class ExpressionType {
-  CONSTANT,
-  COMBINATION,
-  VARIABLE
+  CONSTANT, COMBINATION, VARIABLE
 };
 
 class ExpressionNode : public Node {
  public:
   [[nodiscard]] virtual ExpressionType GetExpressionType() = 0;
   [[nodiscard]] virtual bool operator==(const ExpressionNode &other) const = 0;
-  virtual std::string Accept(DesignExtractor *de, std::string proc_name, bool is_uses) = 0;
+  virtual String Accept(DesignExtractor *de, String proc_name, bool is_uses) = 0;
 };
 
 }
 
-#endif //SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_EXPRESSION_H_
+#endif //NODE_EXPRESSION_H
