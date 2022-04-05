cd83275210509fcc95a088a1f7a54f127af79c8a
Deyi Tan <deyixtan@gmail.com>
2022-04-06 00:07:19 +0800

    Update VariableNode defs

--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_variable.cpp
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_variable.cpp
@@ -5,9 +5,9 @@ namespace source {
 
 VariableNode::VariableNode() : m_identifier("") {}
 
-VariableNode::VariableNode(std::string identifier, std::string stmt) : m_identifier(identifier), m_stmt(stmt) {}
+VariableNode::VariableNode(String identifier, String stmt) : m_identifier(identifier), m_stmt(stmt) {}
 
-std::string VariableNode::GetIdentifier() {
+String VariableNode::GetIdentifier() {
   return m_identifier;
 }
 
@@ -15,22 +15,18 @@ ExpressionType VariableNode::GetExpressionType() {
   return ExpressionType::VARIABLE;
 }
 
-std::string VariableNode::ToString() {
-  return "$" + m_identifier;
+String VariableNode::GetPatternFormat() {
+  return "(" + m_identifier + ")";
 }
 
-std::string VariableNode::GetPatternFormat() {
-  return "(" + m_identifier + ")";
+String VariableNode::Accept(DesignExtractor *de, String proc_name, bool is_uses) {
+  de->GetPkbClient()->PopulateVars(de->GetVisited(), m_stmt, proc_name, m_identifier, is_uses);
+  return GetPatternFormat();
 }
 
 bool VariableNode::operator==(const ExpressionNode &other) const {
   const auto casted_other = dynamic_cast<const VariableNode *>(&other);
   return m_identifier == casted_other->m_identifier;
-}
-
-std::string VariableNode::Accept(DesignExtractor *de, std::string proc_name, bool is_uses) {
-  de->GetPkbClient()->PopulateVars(de->GetVisited(), m_stmt, proc_name, m_identifier, is_uses);
-  return GetPatternFormat();
 }
 
 }
--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_variable.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_variable.h
@@ -1,26 +1,26 @@
-#ifndef SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_VARIABLE_H_
-#define SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_VARIABLE_H_
+#ifndef NODE_VARIABLE_H
+#define NODE_VARIABLE_H
 
+#include "components/source_subsystem/source_declarations.h"
 #include "node_expression.h"
 
 namespace source {
 
 class VariableNode : public ExpressionNode {
  private:
-  std::string m_identifier;
-  std::string m_stmt;
+  String m_identifier;
+  String m_stmt;
 
  public:
   VariableNode();
-  explicit VariableNode(std::string identifier, std::string stmt);
-  [[nodiscard]] std::string GetIdentifier();
+  explicit VariableNode(String identifier, String stmt);
+  [[nodiscard]] String GetIdentifier();
   [[nodiscard]] ExpressionType GetExpressionType() override;
-  [[nodiscard]] std::string ToString() override;
-  [[nodiscard]] std::string GetPatternFormat() override;
+  [[nodiscard]] String GetPatternFormat() override;
+  String Accept(DesignExtractor *de, String proc_name, bool is_uses);
   [[nodiscard]] bool operator==(const ExpressionNode &other) const override;
-  std::string Accept(DesignExtractor *de, std::string proc_name, bool is_uses);
 };
 
 }
 
-#endif //SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_VARIABLE_H_
+#endif //NODE_VARIABLE_H
