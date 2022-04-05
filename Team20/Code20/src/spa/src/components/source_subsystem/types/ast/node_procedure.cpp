1105d84d9ecb569e7ecf8576bd0b67a59935ddcf
Deyi Tan <deyixtan@gmail.com>
2022-04-05 22:55:29 +0800

    Update ProcedureNode defs

--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_procedure.cpp
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_procedure.cpp
@@ -4,32 +4,27 @@
 #include "../../iterator/cfg_builder.h"
 #include "node_statement_list.h"
 #include "node_statement.h"
-#include "../cfg/cfg_node.h"
 
 namespace source {
 
-ProcedureNode::ProcedureNode(std::string identifier, std::shared_ptr<StatementListNode> stmt_list) :
+ProcedureNode::ProcedureNode(String identifier, StatementListNodePtr stmt_list) :
     m_identifier(identifier), m_stmt_list(stmt_list) {}
 
-std::string ProcedureNode::GetIdentifier() {
+String ProcedureNode::GetIdentifier() {
   return m_identifier;
 }
 
-std::shared_ptr<StatementListNode> ProcedureNode::GetStatementList() {
+StatementListNodePtr ProcedureNode::GetStatementList() {
   return m_stmt_list;
 }
 
-std::string ProcedureNode::ToString() {
-  return "procedure " + m_identifier + " {\n" + m_stmt_list->ToString() + "}\n";
-}
-
-std::string ProcedureNode::GetPatternFormat() {
+String ProcedureNode::GetPatternFormat() {
   return "";
 }
 
 bool ProcedureNode::operator==(const ProcedureNode &other) const {
-  std::vector<std::shared_ptr<StatementNode>> this_stmt_list = m_stmt_list->GetStatements();
-  std::vector<std::shared_ptr<StatementNode>> other_stmt_list = other.m_stmt_list->GetStatements();
+  StatementNodeStream this_stmt_list = m_stmt_list->GetStatements();
+  StatementNodeStream other_stmt_list = other.m_stmt_list->GetStatements();
   if (this_stmt_list.size() != other_stmt_list.size()) {
     return false;
   }
@@ -45,7 +40,7 @@ void ProcedureNode::Accept(DesignExtractor *de) {
   de->Visit(m_stmt_list, m_identifier);
 }
 
-std::shared_ptr<CfgNode> ProcedureNode::Accept(CfgBuilder *cb, std::shared_ptr<CfgNode> cfg_node) {
+CfgNodePtr ProcedureNode::Accept(CfgBuilder *cb, CfgNodePtr cfg_node) {
   return cb->Visit(m_stmt_list, cfg_node);
 }
 
--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_procedure.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_procedure.h
@@ -1,26 +1,26 @@
-#ifndef SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_PROCEDURE_H_
-#define SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_PROCEDURE_H_
+#ifndef NODE_PROCEDURE_H
+#define NODE_PROCEDURE_H
 
+#include "components/source_subsystem/source_declarations.h"
 #include "node.h"
 
 namespace source {
 
 class ProcedureNode : public Node {
  private:
-  std::string m_identifier;
-  std::shared_ptr<StatementListNode> m_stmt_list;
+  String m_identifier;
+  StatementListNodePtr m_stmt_list;
 
  public:
-  ProcedureNode(std::string identifier, std::shared_ptr<StatementListNode> stmt_list);
-  [[nodiscard]] std::string GetIdentifier();
-  [[nodiscard]] std::shared_ptr<StatementListNode> GetStatementList();
-  [[nodiscard]] std::string ToString() override;
-  [[nodiscard]] std::string GetPatternFormat() override;
+  ProcedureNode(String identifier, StatementListNodePtr stmt_list);
+  [[nodiscard]] String GetIdentifier();
+  [[nodiscard]] StatementListNodePtr GetStatementList();
+  [[nodiscard]] String GetPatternFormat() override;
+  CfgNodePtr Accept(CfgBuilder *cb, CfgNodePtr cfg_node);
   [[nodiscard]] bool operator==(const ProcedureNode &other) const;
   void Accept(DesignExtractor *de);
-  std::shared_ptr<CfgNode> Accept(CfgBuilder *cb, std::shared_ptr<CfgNode> cfg_node);
 };
 
 }
 
-#endif //SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_PROCEDURE_H_
+#endif //NODE_PROCEDURE_H
