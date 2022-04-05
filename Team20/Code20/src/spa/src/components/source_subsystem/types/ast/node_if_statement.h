a08ccaf952acb13b8fd7cef9482a49196b5c6fe1
Deyi Tan <deyixtan@gmail.com>
2022-04-05 23:44:53 +0800

    Update CfgNodePtr defs

--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_if_statement.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_if_statement.h
@@ -1,36 +1,33 @@
-#ifndef SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_IF_STATEMENT_H_
-#define SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_IF_STATEMENT_H_
+#ifndef NODE_IF_STATEMENT_H
+#define NODE_IF_STATEMENT_H
 
+#include "components/source_subsystem/source_declarations.h"
 #include "node_statement.h"
-#include "node_conditional_expression.h"
-#include "node_statement_list.h"
-#include "components/source_subsystem/pkb_client.h"
 
 namespace source {
 
 class IfStatementNode : public StatementNode {
  private:
-  std::shared_ptr<ConditionalExpressionNode> m_condition;
-  std::shared_ptr<StatementListNode> m_if_stmt_list;
-  std::shared_ptr<StatementListNode> m_else_stmt_list;
+  ConditionalExpressionNodePtr m_condition;
+  StatementListNodePtr m_if_stmt_list;
+  StatementListNodePtr m_else_stmt_list;
 
  public:
-  IfStatementNode(int stmt_no,
-                  std::shared_ptr<ConditionalExpressionNode> condition,
-                  std::shared_ptr<StatementListNode> if_stmt_list,
-                  std::shared_ptr<StatementListNode> else_stmt_list);
-  [[nodiscard]] std::shared_ptr<ConditionalExpressionNode> GetCondition();
-  [[nodiscard]] std::shared_ptr<StatementListNode> GetIfStatementList();
-  [[nodiscard]] std::shared_ptr<StatementListNode> GetElseStatementList();
-  [[nodiscard]] std::vector<std::shared_ptr<StatementNode>> GetAllStatementList();
+  IfStatementNode(String stmt_no,
+                  ConditionalExpressionNodePtr condition,
+                  StatementListNodePtr if_stmt_list,
+                  StatementListNodePtr else_stmt_list);
+  [[nodiscard]] ConditionalExpressionNodePtr GetCondition();
+  [[nodiscard]] StatementListNodePtr GetIfStatementList();
+  [[nodiscard]] StatementListNodePtr GetElseStatementList();
+  [[nodiscard]] StatementNodeStream GetAllStatementList();
   [[nodiscard]] StmtType GetStatementType() override;
-  [[nodiscard]] std::string ToString() override;
-  [[nodiscard]] std::string GetPatternFormat() override;
+  [[nodiscard]] String GetPatternFormat() override;
   [[nodiscard]] bool operator==(const StatementNode &other) const override;
-  void Accept(DesignExtractor *de, std::string proc_name);
-  std::shared_ptr<CfgNode> Accept(CfgBuilder *cb, std::shared_ptr<CfgNode> cfg_node);
+  void Accept(DesignExtractor *de, String proc_name);
+  CfgNodePtr Accept(CfgBuilder *cb, CfgNodePtr cfg_node);
 };
 
 }
 
-#endif //SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_IF_STATEMENT_H_
+#endif //NODE_IF_STATEMENT_H
