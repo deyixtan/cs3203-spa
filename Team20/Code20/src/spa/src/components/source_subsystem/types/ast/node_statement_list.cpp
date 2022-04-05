34bfcf9c752406f26743c46ba23856f6f90efd62
Deyi Tan <deyixtan@gmail.com>
2022-04-05 23:00:19 +0800

    Update StatementListNode defs

--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_statement_list.cpp
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_statement_list.cpp
@@ -7,46 +7,27 @@
 
 namespace source {
 
-StatementListNode::StatementListNode() : m_statements(std::vector<std::shared_ptr<StatementNode >>()) {}
+StatementListNode::StatementListNode() : m_statements(StatementNodeStream()) {}
 
-StatementListNode::StatementListNode(std::vector<std::shared_ptr<StatementNode>> statements)
+StatementListNode::StatementListNode(StatementNodeStream statements)
     : m_statements(statements) {}
 
-std::vector<std::shared_ptr<StatementNode>> StatementListNode::GetStatements() {
+StatementNodeStream StatementListNode::GetStatements() {
   return m_statements;
 }
 
-std::string StatementListNode::ToString() {
-  std::string str = "";
-  for (std::shared_ptr<StatementNode> statement : m_statements) {
-    str += statement->ToString();
-  }
-  return str;
-}
-
-std::string StatementListNode::GetPatternFormat() {
+String StatementListNode::GetPatternFormat() {
   return "";
 }
 
-bool StatementListNode::operator==(const StatementListNode &other) const {
-  if (m_statements.size() != other.m_statements.size()) {
-    return false;
-  }
-  for (int i = 0; i < m_statements.size(); i++) {
-    if (*(m_statements.at(i)) == *(other.m_statements.at(i))) {}
-    else { return false; }
-  }
-  return true;
-}
-
-void StatementListNode::Accept(DesignExtractor *de, std::string proc_name) {
-  std::vector<std::shared_ptr<StatementNode>> stmts = m_statements;
+void StatementListNode::Accept(DesignExtractor *de, String proc_name) {
+  StatementNodeStream stmts = m_statements;
 
   for (int i = 0; i < stmts.size(); ++i) {
-    std::shared_ptr<StatementNode> stmt = stmts[i];
+    StatementNodePtr stmt = stmts[i];
     de->Visit(stmt, proc_name);
-    std::string stmt_num = std::to_string(stmt->GetStatementNumber());
-    std::string var_name = "";
+    String stmt_num = std::to_string(stmt->GetStatementNumber());
+    String var_name = "";
     if (de->GetVisited().size() > 0) {
       de->GetPkbClient()->PopulateParent(de->GetVisited().back(), stmt_num);
     }
@@ -65,17 +46,28 @@ void StatementListNode::Accept(DesignExtractor *de, std::string proc_name) {
   }
 }
 
-std::shared_ptr<CfgNode> StatementListNode::Accept(CfgBuilder *cb, std::shared_ptr<CfgNode> cfg_node) {
-  for (std::shared_ptr<StatementNode> stmt : m_statements) {
+CfgNodePtr StatementListNode::Accept(CfgBuilder *cb, CfgNodePtr cfg_node) {
+  for (StatementNodePtr stmt : m_statements) {
     bool is_empty_cfg_node = cfg_node->GetStatementList().size() == 0;
     if ((stmt->GetStatementType() == IF || stmt->GetStatementType() == WHILE) && !is_empty_cfg_node) {
-      std::shared_ptr<CfgNode> condition_node = std::make_shared<CfgNode>();
+      CfgNodePtr condition_node = std::make_shared<CfgNode>();
       cfg_node->AddNext(condition_node);
       cfg_node = condition_node;
     }
     cfg_node = cb->Visit(stmt, cfg_node);
   }
   return cfg_node;
+}
+
+bool StatementListNode::operator==(const StatementListNode &other) const {
+  if (m_statements.size() != other.m_statements.size()) {
+    return false;
+  }
+  for (int i = 0; i < m_statements.size(); i++) {
+    if (*(m_statements.at(i)) == *(other.m_statements.at(i))) {}
+    else { return false; }
+  }
+  return true;
 }
 
 }
--- Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_statement_list.h
+++ Team20/Code20/src/spa/src/components/source_subsystem/types/ast/node_statement_list.h
@@ -1,25 +1,25 @@
-#ifndef SPA_SRC_SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_STATEMENT_LIST_H_
-#define SPA_SRC_SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_STATEMENT_LIST_H_
+#ifndef STATEMENT_LIST_H
+#define STATEMENT_LIST_H
 
+#include "components/source_subsystem/source_declarations.h"
 #include "node.h"
 
 namespace source {
 
 class StatementListNode : public Node {
  private:
-  std::vector<std::shared_ptr<StatementNode>> m_statements;
+  StatementNodeStream m_statements;
 
  public:
   StatementListNode();
-  StatementListNode(std::vector<std::shared_ptr<StatementNode>> statements);
-  [[nodiscard]] std::vector<std::shared_ptr<StatementNode>> GetStatements();
-  [[nodiscard]] std::string ToString() override;
-  [[nodiscard]] std::string GetPatternFormat() override;
+  explicit StatementListNode(StatementNodeStream statements);
+  [[nodiscard]] StatementNodeStream GetStatements();
+  [[nodiscard]] String GetPatternFormat() override;
+  void Accept(DesignExtractor *de, String proc_name);
+  CfgNodePtr Accept(CfgBuilder *cb, CfgNodePtr cfg_node);
   [[nodiscard]] bool operator==(const StatementListNode &other) const;
-  void Accept(DesignExtractor *de, std::string proc_name);
-  std::shared_ptr<CfgNode> Accept(CfgBuilder *cb, std::shared_ptr<CfgNode> cfg_node);
 };
 
 }
 
-#endif //SPA_SRC_SPA_SRC_COMPONENTS_SOURCE_SUBSYSTEM_AST_NODE_STATEMENT_LIST_H_
+#endif //STATEMENT_LIST_H
