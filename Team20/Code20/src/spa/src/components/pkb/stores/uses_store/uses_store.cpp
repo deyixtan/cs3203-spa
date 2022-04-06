2e4ce3e4e8faea895edec373033e88eca3411036
Jaryl Loh <jarylloh1997@gmail.com>
2022-04-07 00:31:12 +0800

    Typedef uses_store

--- Team20/Code20/src/spa/src/components/pkb/stores/uses_store/uses_store.cpp
+++ Team20/Code20/src/spa/src/components/pkb/stores/uses_store/uses_store.cpp
@@ -3,22 +3,22 @@
 UsesStore::UsesStore(std::shared_ptr<std::vector<std::unordered_set<std::string>>> stmt_vector, std::shared_ptr<std::unordered_map<std::string, StmtType>> stmt_type) :
     StmtVarStore(move(stmt_vector), move(stmt_type)) {}
 
-std::unordered_set<std::string> UsesStore::GetVarUsedByStmt(std::string const &stmt) {
+UsesStore::IDENT_SET UsesStore::GetVarUsedByStmt(std::string const &stmt) {
   return GetVarByStmt(stmt);
 }
 
-std::unordered_set<std::string> UsesStore::GetStmtUsedByVar(StmtType type, std::string const &var) {
+UsesStore::IDENT_SET UsesStore::GetStmtUsedByVar(StmtType type, std::string const &var) {
   return GetStmtByVar(type, var);
 }
 
-std::unordered_set<std::string> UsesStore::GetAllStmtUsing() {
+UsesStore::IDENT_SET UsesStore::GetAllStmtUsing() {
   return GetAllStmt();
 }
 
-std::unordered_set<std::string> UsesStore::GetAllProcUsing() {
+UsesStore::IDENT_SET UsesStore::GetAllProcUsing() {
   return GetAllProc();
 }
 
-std::unordered_set<std::pair<std::string, std::string>, pair_hash> UsesStore::GetAllUsesStmt(StmtType type) {
+UsesStore::IDENT_PAIR_SET UsesStore::GetAllUsesStmt(StmtType type) {
   return GetPairByType(type);
 }
