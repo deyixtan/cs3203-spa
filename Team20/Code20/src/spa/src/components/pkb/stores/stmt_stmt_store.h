bb2bcf67ffec248c49fd59222368484795819525
Jaryl Loh <jarylloh1997@gmail.com>
2022-04-06 23:56:16 +0800

    Typedef stmt_stmt_store

--- Team20/Code20/src/spa/src/components/pkb/stores/stmt_stmt_store.h
+++ Team20/Code20/src/spa/src/components/pkb/stores/stmt_stmt_store.h
@@ -5,111 +5,77 @@
 
 class StmtStmtStore : public Store {
  private:
-  std::unordered_map<StmtType,
-                     std::unordered_map<StmtType,
-                                        std::tuple<std::unordered_map<std::string,
-                                                                      std::unordered_set<std::string>>,
-                                                   std::unordered_map<std::string,
-                                                                      std::unordered_set<std::string>>,
-                                                   std::unordered_set<std::pair<std::string,
-                                                                                std::string>,
-                                                                      pair_hash>>>>
-      type_pair_map;
-  std::unordered_map<StmtType,
-                     std::unordered_map<StmtType,
-                                        std::tuple<std::unordered_map<std::string,
-                                                                      std::unordered_set<std::string>>,
-                                                   std::unordered_map<std::string,
-                                                                      std::unordered_set<std::string>>,
-                                                   std::unordered_set<std::pair<std::string,
-                                                                                std::string>,
-                                                                      pair_hash>>>>
-      star_type_pair_map;
-  std::unordered_set<std::pair<std::string, std::string>, pair_hash> all_pairs;
-  std::unordered_set<std::pair<std::string, std::string>, pair_hash> all_star_pairs;
+  NESTED_MAP type_pair_map;
+  NESTED_MAP star_type_pair_map;
+  STMT_PAIR_SET all_pairs;
+  STMT_PAIR_SET all_star_pairs;
 
  public:
   explicit StmtStmtStore(std::shared_ptr<std::vector<std::unordered_set<std::string>>> stmt_vector,
                          std::shared_ptr<std::unordered_map<std::string, StmtType>> stmt_type);
   void AddUpperLower(StoreType store_type,
-                     std::string const &upper,
-                     std::string const &lower);
+                     STMT const &upper,
+                     STMT const &lower);
   void AddUpperLowerStar(StoreType store_type,
-                         std::string const &upper,
-                         std::string const &lower,
-                         std::vector<std::string> const &visited);
-  void AddFollows(bool is_star, StmtType type1, std::string const &upper, StmtType type2, std::string const &lower);
+                         STMT const &upper,
+                         STMT const &lower,
+                         STMT_VECTOR const &visited);
+  void AddFollows(bool is_star, StmtType type1, STMT const &upper, StmtType type2, STMT const &lower);
   void AddParent(bool is_star,
-                 std::string const &upper,
-                 std::string const &lower,
-                 std::vector<std::string> const &visited);
-  void AddCalls(bool is_star, std::string const &upper, std::string const &lower);
-  void AddNext(bool is_star, StmtType type1, std::string const &upper, StmtType type2, std::string const &lower);
+                 STMT const &upper,
+                 STMT const &lower,
+                 STMT_VECTOR const &visited);
+  void AddCalls(bool is_star, STMT const &upper, STMT const &lower);
+  void AddNext(bool is_star, StmtType type1, STMT const &upper, StmtType type2, STMT const &lower);
   void PopulatePairMap(StmtType type1,
-                       std::string upper,
+                       STMT upper,
                        StmtType type2,
-                       std::string lower,
-                       std::unordered_map<StmtType,
-                                          std::unordered_map<StmtType,
-                                                             std::tuple<std::unordered_map<std::string,
-                                                                                           std::unordered_set<std::string>>,
-                                                                        std::unordered_map<std::string,
-                                                                                           std::unordered_set<std::string>>,
-                                                                        std::unordered_set<std::pair<std::string,
-                                                                                                     std::string>,
-                                                                                           pair_hash>>>> *pair_map);
+                       STMT lower,
+                       NESTED_MAP *pair_map);
   void ExhaustiveAddAllStmt(StmtType type1, std::string upper, StmtType type2, std::string lower, bool is_star);
   void ExhaustiveAddSubStmt(StmtType type1,
-                            std::string upper,
+                            STMT upper,
                             StmtType type2,
-                            std::string lower,
-                            std::unordered_map<StmtType,
-                                               std::unordered_map<StmtType,
-                                                                  std::tuple<std::unordered_map<std::string,
-                                                                                                std::unordered_set<std::string>>,
-                                                                             std::unordered_map<std::string,
-                                                                                                std::unordered_set<std::string>>,
-                                                                             std::unordered_set<std::pair<std::string,
-                                                                                                          std::string>,
-                                                                                                pair_hash>>>> *pair_map);
+                            STMT lower,
+                            NESTED_MAP *pair_map);
   void WipeNextStar();
-  [[nodiscard]] bool IsValid(std::pair<std::string, std::string> const &pair);
-  [[nodiscard]] bool IsStarValid(std::pair<std::string, std::string> const &pair);
-  [[nodiscard]] bool IsNextStarPairValid(std::pair<std::string, std::string> const &pair);
+  [[nodiscard]] bool IsValid(STMT_PAIR const &pair);
+  [[nodiscard]] bool IsStarValid(STMT_PAIR const &pair);
+  [[nodiscard]] bool IsNextStarPairValid(STMT_PAIR const &pair);
   [[nodiscard]] std::unordered_set<std::string> GetHelper(StmtType type1,
                                                           StmtType type2,
                                                           int index,
-                                                          std::string const &stmt,
+                                                          STMT const &stmt,
                                                           bool is_star);
-  [[nodiscard]] std::string GetUpperOf(StmtType stmt_type, std::string const &stmt);
-  [[nodiscard]] std::string GetLowerOf(StmtType stmt_type, std::string const &stmt);
-  [[nodiscard]] std::unordered_set<std::string> GetUpperSetOf(StoreType store_type,
-                                                              StmtType stmt_type,
-                                                              std::string const &stmt);
-  [[nodiscard]] std::unordered_set<std::string> GetLowerSetOf(StoreType store_type,
-                                                              StmtType stmt_type,
-                                                              std::string const &stmt);
-  [[nodiscard]] std::unordered_set<std::string> GetUpperStarOf(StoreType store_type,
-                                                               StmtType stmt_type,
-                                                               std::string const &stmt);
-  [[nodiscard]] std::unordered_set<std::string> GetLowerStarOf(StoreType store_type,
-                                                               StmtType stmt_type,
-                                                               std::string const &stmt);
-  [[nodiscard]] std::unordered_set<std::pair<std::string, std::string>, pair_hash> GetPairByType(StmtType type1,
-                                                                                                 StmtType type2);
-  [[nodiscard]] std::unordered_set<std::pair<std::string, std::string>, pair_hash> GetStarPairByType(StmtType type1,
-                                                                                                     StmtType type2);
-  [[nodiscard]] std::unordered_set<std::pair<std::string, std::string>, pair_hash> GetAllPairs();
-  [[nodiscard]] std::unordered_set<std::pair<std::string, std::string>, pair_hash> GetAllStarPairs();
-  [[nodiscard]] std::unordered_set<std::pair<std::string, std::string>, pair_hash> GetAllNextStarPairs();
+  [[nodiscard]] STMT GetUpperOf(StmtType stmt_type, STMT const &stmt);
+  [[nodiscard]] STMT GetLowerOf(StmtType stmt_type, STMT const &stmt);
+  [[nodiscard]] STMT_SET GetUpperSetOf(StoreType store_type,
+                                       StmtType stmt_type,
+                                       STMT const &stmt);
+  [[nodiscard]] STMT_SET GetLowerSetOf(StoreType store_type,
+                                       StmtType stmt_type,
+                                       STMT const &stmt);
+  [[nodiscard]] STMT_SET GetUpperStarOf(StoreType store_type,
+                                        StmtType stmt_type,
+                                        STMT const &stmt);
+  [[nodiscard]] STMT_SET GetLowerStarOf(StoreType store_type,
+                                        StmtType stmt_type,
+                                        STMT const &stmt);
+  [[nodiscard]] STMT_PAIR_SET GetPairByType(StmtType type1,
+                                            StmtType type2);
+  [[nodiscard]] STMT_PAIR_SET GetStarPairByType(StmtType type1,
+                                                StmtType type2);
+  [[nodiscard]] STMT_PAIR_SET GetAllPairs();
+  [[nodiscard]] STMT_PAIR_SET GetAllStarPairs();
+  [[nodiscard]] STMT_PAIR_SET GetAllNextStarPairs();
   void GetLowerStarOfHelper(StmtType stmt_type,
-                                          std::string const &stmt,
-                                          std::unordered_set<std::string> &res,
-                                          std::unordered_set<std::string> &visited);
+                            STMT const &stmt,
+                            STMT_SET &res,
+                            STMT_SET &visited);
   void GetUpperStarOfHelper(StmtType stmt_type,
-                                          std::string const &stmt,
-                                          std::unordered_set<std::string> &res,
-                                          std::unordered_set<std::string> &visited);
+                            STMT const &stmt,
+                            STMT_SET &res,
+                            STMT_SET &visited);
 };
 
 #endif //FOLLOWS_PARENT_STORE_H
