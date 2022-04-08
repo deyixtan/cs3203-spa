3db34138682c4ef9f7d513555138d2fce839c44d
Jaryl Loh <jarylloh1997@gmail.com>
2022-04-09 04:20:15 +0800

    Update relationship object:

--- Team20/Code20/src/spa/src/components/pkb/relationship.h
+++ Team20/Code20/src/spa/src/components/pkb/relationship.h
@@ -12,13 +12,16 @@ class Relationship {
  private:
   IDENT_SET_MAP upper_to_lower_set;
   IDENT_SET_MAP lower_to_upper_set;
-  IDENT_PAIR_VECTOR pair_set;
+  IDENT_PAIR_VECTOR pair_vector;
 
  public:
   Relationship();
+  void AddLowerSet(IDENT stmt, IDENT_SET set);
+  void AddUpperSet(IDENT stmt, IDENT_SET set);
+  void AddPair(IDENT stmt1, IDENT stmt2);
   [[nodiscard]] IDENT_SET_MAP GetUpperToLowerSet();
   [[nodiscard]] IDENT_SET_MAP GetLowerToUpperSet();
-  [[nodiscard]] IDENT_PAIR_VECTOR GetPairSet();
+  [[nodiscard]] IDENT_PAIR_VECTOR GetPairVector();
 };
 
 #endif //RELATIONSHIP_H
