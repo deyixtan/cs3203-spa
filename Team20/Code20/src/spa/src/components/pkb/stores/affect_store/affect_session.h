712c8c7484924f516fc7560cc43c51bd754e4d81
Deyi Tan <deyixtan@gmail.com>
2022-04-09 20:11:32 +0800

    Fix AffectSession notes

--- Team20/Code20/src/spa/src/components/pkb/stores/affect_store/affect_session.h
+++ Team20/Code20/src/spa/src/components/pkb/stores/affect_store/affect_session.h
@@ -64,27 +64,3 @@ class AffectSession {
 };
 
 #endif //AFFECT_SESSION_H
-
-// Notes:
-
-// Affects(_, _)
-
-// Affects(a, _)
-// Affects(_, a)
-// Affects(a1, a2)
-
-// Affects(s, _)
-// Affects(_, s)
-// Affects(s1, s2)
-
-// Affects(a, s)
-// Affects(s, a)
-
-// Affects(1, _)
-// Affects(_, 1)
-// Affects(1, 2)
-
-// Affects(1, s)
-// Affects(s, 1)
-// Affects(1, a)
-// Affects(a, 1)
